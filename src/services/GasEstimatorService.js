c07ffed5478e7e2da86bd95a4ea312dd2a34c4b5
Tyler Sorensen <Tyler@tylers-mbp.lan>
2018-02-07 14:02:29 -0800

    added a comment on how to debug

--- src/services/GasEstimatorService.js
+++ src/services/GasEstimatorService.js
@@ -11,21 +11,6 @@ export default class GasEstimatorService extends PublicService {
     this._absolute = null;
   }
 
-//version that works
-/*
-  //0xJs has a constant number of 200,000 that it adds to gas estimates as a buffer, as opposed to a percentage-based buffer
-  //should make it so that if the gas would be over the block gas limit, the max allowable gas is returned instead.  Should also test for this
-  estimateGasLimit(transaction){
-    if (this._percentage == null && this._absolute == null) { throw new Error("no gas limit policy set"); } //0xJS calls estimateGas and addes 200000 if no gas limit provided as of last November: https://github.com/0xProject/0x.js/blob/46ad7b1b38df0f302821258629ffa749e7dd00b9/packages/0x.js/CHANGELOG.md
-    if (this._percentage == null && this._absolute != null) { return this._absolute; }
-    return this.get('web3').eth.estimateGas(transaction)
-    .then((estimate) => {
-      if (this._absolute == null) { return estimate * this._percentage; }
-      return Math.min(estimate * this._percentage, this._absolute);
-    })
-  }
-*/
-
     //0xJs has a constant number of 200,000 that it adds to gas estimates as a buffer, as opposed to a percentage-based buffer
   //should make it so that if the gas would be over the block gas limit, the max allowable gas is returned instead.  Should also test for this
   estimateGasLimit(transaction){
@@ -34,7 +19,7 @@ export default class GasEstimatorService extends PublicService {
     return this.get('web3').eth.getBlock("latest")
     .then( block => {
       blockLimit = block.gasLimit;
-      if (this._percentage == null && this._absolute != null) { return Math.min(this._absolute, blockLimit); }
+      if (this._percentage == null && this._absolute != null) { return Math.min(this._absolute, blockLimit); } //to debug, try having this return the 2XXXX number
       return this.get('web3').eth.estimateGas(transaction)
     })
     .then(estimate => {
