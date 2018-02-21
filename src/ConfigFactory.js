79eb40323c83665e976e7f490225d0d1d65941c5
Wouter Kampmann <wouter@ksdc.be>
2018-02-21 18:34:17 +0100

    Trivial ConfigFactory implementation.

---
+++ src/ConfigFactory.js
@@ -0,0 +1,28 @@
+import decentralizedOasisWithoutProxies from './configs/decentralized-oasis-without-proxies';
+
+/**
+ *
+ */
+class ConfigPresetNotFoundError extends Error {
+  constructor(message) {
+    super('Cannot find configuration preset with name: ' + message);
+  }
+}
+
+/**
+ *
+ */
+export default class ConfigFactory {
+
+  /**
+   * @param {string} presetName
+   */
+  static create(presetName) {
+    switch (presetName) {
+    case 'decentralized-oasis-without-proxies':
+      return decentralizedOasisWithoutProxies;
+    default:
+      throw new ConfigPresetNotFoundError(presetName);
+    }
+  }
+}
---
+++ src/ConfigFactory.spec.js
@@ -0,0 +1,11 @@
+import decentralizedOasisWithoutProxies from './configs/decentralized-oasis-without-proxies';
+import ConfigFactory from './ConfigFactory';
+
+test('should return a preset by name', () => {
+  expect(ConfigFactory.create('decentralized-oasis-without-proxies')).toEqual(decentralizedOasisWithoutProxies);
+});
+
+test('should throw an error when requesting a non-existing preset', () => {
+  expect(() => ConfigFactory.create('does-not-exist'))
+    .toThrow('Cannot find configuration preset with name: does-not-exist');
+});
