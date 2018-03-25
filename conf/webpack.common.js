a4db02370a0d9d2a426ead89beeaf42bbd2d8a97
Ethan Bennett <ebennett19@gmail.com>
2018-03-25 13:34:06 -0600

    Fix exports

--- conf/webpack.common.js
+++ conf/webpack.common.js
@@ -4,61 +4,77 @@ const CleanWebpackPlugin = require('clean-webpack-plugin');
 const ExtractTextPlugin = require('extract-text-webpack-plugin');
 
 const extractSass = new ExtractTextPlugin({
-  filename: '[name].[contenthash].css'
+  filename: '[name].[contenthash].css',
 });
 
 module.exports = {
   context: path.join(process.cwd(), 'web'), //the home directory for webpack
   devtool: 'source-map', // enhance debugging by adding meta info for the browser devtools
   entry: {
-    app: '../src/index.js'
+    app: '../src/index.js',
   },
   output: {
     path: path.join(process.cwd(), 'dist'),
     filename: '[name].[hash].js',
     publicPath: '/',
-    sourceMapFilename: '[name].map'
+    sourceMapFilename: '[name].map',
+    library: 'ConfigFactory',
+    libraryTarget: 'umd',
+  },
+  resolve: {
+    extensions: ['.js', '.json'],
   },
   module: {
     noParse: [/dtrace-provider/, /safe-json-stringify/, /mv/],
-    rules: [{
-      enforce: 'pre', //to check source files, not modified by other loaders (like babel-loader)
-      test: /\.js$/,
-      exclude: /node_modules/,
-      loader: 'eslint-loader'
-    }, {
-      test: /\.js$/,
-      exclude: /node_modules/,
-      use: {
-        loader: 'babel-loader',
-        options: {
-          presets: ['env', 'stage-2']
-        }
-      }
-    },{
-      test: /\.scss$/,
-      use: extractSass.extract({
-        use: [{
-          loader: 'css-loader',
+    rules: [
+      {
+        enforce: 'pre', //to check source files, not modified by other loaders (like babel-loader)
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: 'eslint-loader',
+      },
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
           options: {
-            sourceMap: true
-          }
-        }, {
-          loader: 'sass-loader',
-          options: {
-            sourceMap: true
-          }
-        }],
-        // use style-loader in development
-        fallback: 'style-loader'
-      })
-    }]
+            presets: ['env', 'stage-2'],
+          },
+        },
+      },
+      {
+        test: /\.scss$/,
+        use: extractSass.extract({
+          use: [
+            {
+              loader: 'css-loader',
+              options: {
+                sourceMap: true,
+              },
+            },
+            {
+              loader: 'sass-loader',
+              options: {
+                sourceMap: true,
+              },
+            },
+          ],
+          // use style-loader in development
+          fallback: 'style-loader',
+        }),
+      },
+      {
+        test: /\.json$/,
+        loader: 'json-loader',
+      },
+    ],
   },
   plugins: [
-    new CleanWebpackPlugin(['dist'], {root: process.cwd()}),
+    new CleanWebpackPlugin(['dist'], { root: process.cwd() }),
     new webpack.optimize.CommonsChunkPlugin({
-      name: 'vendor'
+      name: 'vendor',
     }),
-    extractSass
-  ]
+    extractSass,
+  ],
 };
--- src/index.js
+++ src/index.js
@@ -1,1 +1,2 @@
 export { default as ConfigFactory } from './utils/ConfigFactory';
+export { default as Web3Service } from './eth/Web3Service';
