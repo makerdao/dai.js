2c59a375a85b7a54fe7a28c3ca2488a70050e73d
Wouter Kampmann <wouter@ksdc.be>
2018-05-05 18:16:14 +0200

    Debug utils.

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,5 +1,42 @@
 import networks from '../../contracts/networks';
 
+export function measure(cb, name, limit = 100) {
+  const start = new Date().getTime(),
+    result = cb(),
+    duration = new Date().getTime() - start;
+
+  if (duration > limit) {
+    console.error((name || 'Callback') + ' took ' + duration + ' ms.');
+  } else {
+    console.log((name || 'Callback') + ' took ' + duration + ' ms.');
+  }
+
+  return result;
+}
+
+let start = null, lastName = 'Timer';
+export const watch = {
+  start: (name = 'Timer') => {
+    start = new Date().getTime();
+    lastName = name;
+    console.warn(name + ' START');
+  },
+  log: (label) => {
+    console.warn(lastName + ' LOG: ' + (label ? label + ' ': '') + (new Date().getTime() - (start || new Date().getTime())) + 'ms.');
+  },
+  pass: (label) => {
+    return (result) => {
+      console.warn(lastName + ' LOG: ' + (label ? label + ' ': '') + (new Date().getTime() - (start || new Date().getTime())) + 'ms.');
+      return result;
+    }
+  },
+  end: () => {
+    console.warn(lastName + ' END: ' + (new Date().getTime() - (start || new Date().getTime())) + 'ms.');
+    start = undefined;
+    lastName = 'Timer';
+  }
+};
+
 export function promisifyAsync(fn) {
   return function() {
     let args = [].slice.call(arguments);
