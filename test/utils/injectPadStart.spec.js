3726ba91c9bc7edc7ab3f96783399bc6ab96b3e3
Wouter Kampmann <wouter@ksdc.be>
2018-05-06 14:23:06 +0200

    Move injectPadStart to polyfills file.

--- test/utils/injectPadStart.spec.js
+++
@@ -1,8 +0,0 @@
-import injectPadStart from '../../src/utils/injectPadStart';
-
-test('Injecting padStart should make the function available in older versions of node', done => {
-  injectPadStart();
-
-  expect('0'.padStart(3, 0)).toBe('000');
-  done();
-});
