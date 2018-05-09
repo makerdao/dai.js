1577cae16adfaccff4cd8cb07521df6b12e141b1
Wouter Kampmann <wouter@ksdc.be>
2018-05-09 21:56:24 +0200

    Promise part of the transaction / business object hybrid.

--- src/eth/TransactionManager.js
+++ src/eth/TransactionManager.js
@@ -1,8 +1,9 @@
-import PrivateService from '../core/PrivateService';
+import PublicService from '../core/PublicService';
 import Web3Service from './Web3Service';
 import TransactionObject from './TransactionObject';
+import TransactionState from './TransactionState';
 
-export default class TransactionManager extends PrivateService {
+export default class TransactionManager extends PublicService {
 
   static buildTestService(web3 = null) {
     web3 = web3 || Web3Service.buildTestService();
@@ -17,5 +18,30 @@ export default class TransactionManager extends PrivateService {
 
   constructor(name = 'transactionManager') {
     super(name, ['web3', 'log']);
+  }
+
+  createTransaction(contractTransaction, businessObject = null, implicitState = TransactionState.mined) {
+    const tx = new TransactionObject(contractTransaction, this.get('web3'), businessObject),
+      hybrid = this._getImplicitStatePromise(tx, implicitState);
+
+    hybrid._original = tx;
+
+    return hybrid;
+  }
+
+  _getImplicitStatePromise(transaction, state) {
+    switch (state) {
+      case TransactionState.pending:
+        return transaction.onPending();
+
+      case TransactionState.mined:
+        return transaction.onMined();
+
+      case TransactionState.finalized:
+        return transaction.onFinalized();
+
+      default:
+        throw new Error('Invalid implicit transaction state: ' + state);
+    }
   }
 }
--- test/eth/TransactionManager.spec.js
+++ test/eth/TransactionManager.spec.js
@@ -1,25 +1,106 @@
-import EthereumTokenService from '../../src/eth/EthereumTokenService';
+import SmartContractService from '../../src/eth/SmartContractService';
 import TransactionManager from '../../src/eth/TransactionManager';
+import TransactionState from '../../src/eth/TransactionState';
+import tokens from '../../contracts/tokens';
 
 function buildTestServices() {
-  const tokenService = EthereumTokenService.buildTestService(),
-    transactionManager = TransactionManager.buildTestService(tokenService.get('web3'));
+  const smartContract = SmartContractService.buildTestService(),
+    transactionManager = TransactionManager.buildTestService(smartContract.get('web3'));
 
   return Promise.all([
-    tokenService.manager().authenticate(),
+    smartContract.manager().authenticate(),
     transactionManager.manager().authenticate()]
   ).then(() => ({
-    token: tokenService,
-    txMgr: transactionManager
+    contract: smartContract,
+    txMgr: transactionManager,
+    defaultAccount: smartContract.get('web3').defaultAccount()
   }));
 }
 
 test('should reuse the same web3 and log service in test services', done => {
   buildTestServices().then(services => {
-    expect(services.token.manager().isAuthenticated()).toBe(true);
-    expect(services.txMgr.manager().isAuthenticated()).toBe(true);
-    expect(services.txMgr.get('web3')).toBe(services.token.get('web3'));
-    expect(services.txMgr.get('log')).toBe(services.token.get('web3').get('log'));
+    expect(services.contract.manager().isConnected()).toBe(true);
+    expect(services.txMgr.manager().isConnected()).toBe(true);
+    expect(services.txMgr.get('web3')).toBe(services.contract.get('web3'));
+    expect(services.txMgr.get('log')).toBe(services.contract.get('web3').get('log'));
+    expect(services.defaultAccount).toMatch(/^0x[0-9A-Fa-f]+$/);
+    done();
+  });
+});
+
+test('should create a Transaction object based on a Contract transaction promise', done => {
+  buildTestServices().then(services => {
+    const contractTransaction = services.contract.getContractByName(tokens.DAI)
+      .approve(services.defaultAccount, '1000000000000000000'),
+      businessObject = { x:1 },
+      hybrid = services.txMgr.createTransaction(contractTransaction, businessObject);
+
+    expect(contractTransaction.toString()).toEqual('[object Promise]');
+    expect(hybrid.toString()).toEqual('[object Promise]');
+    expect(hybrid._original._transaction).toBe(contractTransaction);
+    expect(hybrid._original._businessObject).toBe(businessObject);
+    expect(hybrid._original._web3Service).toBe(services.txMgr.get('web3'));
+
+    hybrid.then(() => {
+      expect(hybrid._original.isMined()).toBe(true);
+      done();
+    });
+  });
+});
+
+test('should resolve the hybrid object when its implicit state is reached', done => {
+  buildTestServices().then(services => {
+    const contract = services.contract.getContractByName(tokens.DAI),
+      contractTransaction =
+        contract.approve(services.defaultAccount, '1000000000000000000'),
+      pendingStatehybrid =
+        services.txMgr.createTransaction(contractTransaction, null, TransactionState.pending),
+      minedStatehybrid =
+        services.txMgr.createTransaction(contractTransaction, null, TransactionState.mined),
+      finalizedStatehybrid =
+        services.txMgr.createTransaction(contractTransaction, null, TransactionState.finalized);
+
+    pendingStatehybrid
+      .then(() => {
+        expect(pendingStatehybrid._original.isPending()).toBe(true);
+        return minedStatehybrid;
+      })
+      .then(() => {
+        expect(pendingStatehybrid._original.isMined()).toBe(true);
+
+        let finished = false;
+        finalizedStatehybrid.then(() => {
+          expect(finalizedStatehybrid._original.isFinalized()).toBe(true);
+          finished = true;
+          done();
+        });
+
+        const RequiredConfirmations = 3; // Update this when the value in TransactionObject changes!!
+        for (let i=0; !finished && i<RequiredConfirmations; i++) {
+          contract.approve(services.defaultAccount, '1000000000000000000');
+        }
+      });
+
+  });
+});
+
+test('should reject invalid implicit states', done => {
+  buildTestServices().then(services => {
+    const contractTransaction = services.contract.getContractByName(tokens.DAI)
+      .approve(services.defaultAccount, '1000000000000000000');
+
+    expect(() =>
+      services.txMgr.createTransaction(contractTransaction, null, TransactionState.initialized))
+      .toThrow('Invalid implicit transaction state: initialized');
+
+    expect(() =>
+      services.txMgr.createTransaction(contractTransaction, null, TransactionState.error))
+      .toThrow('Invalid implicit transaction state: error');
+
+    expect(() =>
+      services.txMgr.createTransaction(contractTransaction, null, 'NOT_A_STATE'))
+      .toThrow('Invalid implicit transaction state: NOT_A_STATE');
+
     done();
   });
 });
