175d7fec46d48d5f5a455abcd0b17f4cd3e67b91
Wouter Kampmann <wouter@ksdc.be>
2018-05-10 22:44:57 +0200

    Plugged in Vue.js for the dev server build.

---
+++ components/MakerDebugger.vue
@@ -0,0 +1,25 @@
+<template>
+    <div class="makerDebugger">
+        <ul>
+            <li v-for="log in logItems">{{log}}</li>
+        </ul>
+    </div>
+</template>
+
+<script>
+  export default {
+    name: "MakerDebugger",
+    data: function() {
+      return {
+        logItems: ['item 1', 'item 2', 'item 3']
+      };
+    }
+  }
+</script>
+
+<style lang="scss" scoped>
+    .makerDebugger {
+        font-size: 80%;
+        color: darkslategrey;
+    }
+</style>
