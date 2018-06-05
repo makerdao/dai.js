8d9430fe090b98ff07474b1f7a2c5a7745d71c4f
Joshua Levine <joshuapark95@gmail.com>
2018-06-05 16:50:16 -0500

    Add 'lockPeth'

--- test/eth/EthereumCdpService.spec.js
+++ test/eth/EthereumCdpService.spec.js
@@ -189,6 +189,41 @@ test(
 );
 
 test(
+  'should be able to lock peth in a cdp',
+  async () => {
+    const id = await openCdp();
+    const tokenService = createdCdpService.get('token');
+    const wethToken = tokenService.getToken(tokens.WETH);
+    const pethToken = tokenService.getToken(tokens.PETH);
+    const defaultAccount = createdCdpService
+      .get('token')
+      .get('web3')
+      .defaultAccount();
+
+    await wethToken.deposit('0.1');
+    await wethToken.approve(
+      createdCdpService._tubContract().getAddress(),
+      '0.1'
+    );
+    await pethToken.join('0.1');
+
+    const balancePre = await pethToken.balanceOf(defaultAccount);
+    const cdpInfoPre = await createdCdpService.getCdpInfo(id);
+    await createdCdpService.lockPeth(id, '0.1');
+    const cdpInfoPost = await createdCdpService.getCdpInfo(id);
+    const balancePost = await pethToken.balanceOf(defaultAccount);
+
+    expect(cdpInfoPre.ink.toString()).toEqual('0');
+    expect(cdpInfoPost.ink.toString()).toEqual('100000000000000000');
+    expect(parseFloat(balancePost)).toBeCloseTo(balancePre - 0.1, 5);
+
+    await wethToken.approve(createdCdpService._tubContract().getAddress(), '0');
+    await pethToken.approve(createdCdpService._tubContract().getAddress(), '0');
+  },
+  5000
+);
+
+test(
   'should be able to free peth from a cdp',
   done => {
     let newCdp;
