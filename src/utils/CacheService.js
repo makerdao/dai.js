d8618f060291d1e4a94821921a6184877e3f88a0
Lawrence Wang <l@lw.io>
2018-06-18 17:05:24 -0700

    disable CacheService in tests

--- src/utils/CacheService.js
+++ src/utils/CacheService.js
@@ -1,24 +1,29 @@
 import LocalService from '../core/LocalService';
 
 export default class CacheService extends LocalService {
-  // TODO pass in global object as storage argument
   constructor(name = 'cache') {
     super(name);
   }
 
   initialize(settings = {}) {
-    this._storage = settings.storage || {};
+    if (settings.storage) {
+      this._storage = settings.storage;
+    }
+  }
+
+  isEnabled() {
+    return !!this._storage;
   }
 
   has(key) {
-    return key in this._storage;
+    return !!this._storage && key in this._storage;
   }
 
   fetch(key) {
-    return this._storage[key];
+    return this._storage ? this._storage[key] : undefined;
   }
 
   store(key, value) {
-    this._storage[key] = value;
+    if (this._storage) this._storage[key] = value;
   }
 }
