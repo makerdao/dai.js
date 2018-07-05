b5a5566b7393d539f4d16ccb03dcb237a2341257
Lawrence Wang <l@lw.io>
2018-07-05 13:03:58 -0700

    clean up Maker spec

--- test/Maker.spec.js
+++ test/Maker.spec.js
@@ -4,66 +4,26 @@ function createMaker() {
   return new Maker('test', { log: false });
 }
 
-test(
-  'openCdp should open a CDP',
-  done => {
-    const maker = createMaker();
-
-    maker
-      .openCdp()
-      .then(cdp => cdp.getId())
-      .then(id => {
-        expect(typeof id).toBe('number');
-        expect(id).toBeGreaterThan(0);
-        done();
-      });
-  },
-  5000
-);
-
-test('should create a new CDP object for existing CDPs', done => {
+test('openCdp', async () => {
   const maker = createMaker();
-  let createdCdp;
 
-  maker.openCdp().then(cdp => {
-    createdCdp = cdp;
-    cdp.getId().then(id => {
-      maker.getCdp(id).then(newCdpObject => {
-        expect(createdCdp.getId()).toEqual(newCdpObject.getId());
-        expect(createdCdp._cdpService).toEqual(newCdpObject._cdpService);
-        expect(createdCdp._smartContractService).toEqual(
-          newCdpObject._smartContractService
-        );
-        done();
-      });
-    });
-  });
+  const cdp = await maker.openCdp();
+  const id = await cdp.getId();
+  expect(typeof id).toBe('number');
+  expect(id).toBeGreaterThan(0);
 });
 
-test('should validate the provided CDP ID', done => {
+test('creates a new CDP object for existing CDPs', async () => {
   const maker = createMaker();
-  let cdpId;
-
-  maker
-    .openCdp()
-    .then(cdp => cdp.getId())
-    .then(id => {
-      cdpId = id;
-      return maker.getCdp(cdpId);
-    })
-    .then(cdp => cdp.getId())
-    .then(fetchedId => {
-      expect(fetchedId).toEqual(cdpId);
-
-      //These crash other unit tests later on, strangely enough
-      //expect(maker.getCdp('a')).rejects.toThrowError('must be a number');
-      //expect(maker.getCdp(8000)).rejects.toThrowError('try opening a new one');
-
-      done();
-    });
+  const cdp = await maker.openCdp();
+  const id = await cdp.getId();
+  const newCdp = await maker.getCdp(id);
+  expect(id).toEqual(await newCdp.getId());
+  expect(cdp._cdpService).toEqual(newCdp._cdpService);
+  expect(cdp._smartContractService).toEqual(newCdp._smartContractService);
 });
 
-test('should throw an error for an invalid id', async () => {
+test('throws an error for an invalid id', async () => {
   const maker = createMaker();
   expect.assertions(1);
   try {
