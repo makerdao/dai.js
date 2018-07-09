780477ff25058346ead547898a543ba3c5cf6b6a
Lawrence Wang <l@lw.io>
2018-07-09 13:42:56 -0700

    add script to show service dependencies

---
+++ scripts/showServiceDependencies.js
@@ -0,0 +1,73 @@
+// run this with babel-node:
+// > npx babel-node scripts/showServiceDependencies.js
+
+import DefaultServiceProvider from '../src/config/DefaultServiceProvider';
+import { defaultServices } from '../src/config';
+import chalk from 'chalk';
+import times from 'lodash.times';
+
+if (!process.env.DEBUG) {
+  process.env.DEBUG = 'makerjs:container';
+}
+
+const config = {};
+for (let key in defaultServices) {
+  config[key] = true;
+}
+
+const colors = [
+  '#e6194b',
+  '#3cb44b',
+  '#ffe119',
+  '#0082c8',
+  '#f58231',
+  '#911eb4',
+  '#46f0f0',
+  '#f032e6',
+  '#d2f53c',
+  '#fabebe',
+  '#008080',
+  '#e6beff',
+  '#aa6e28',
+  '#fffac8',
+  '#800000',
+  '#aaffc3',
+  '#808000',
+  '#ffd8b1',
+  '#000080',
+  '#808080'
+];
+
+const colorMap = {};
+let colorMapCounter = 0;
+
+const colorize = function(serviceName) {
+  if (!colorMap[serviceName]) {
+    if (colorMapCounter >= colors.length) {
+      return chalk.reset(serviceName);
+    }
+    colorMap[serviceName] = colors[colorMapCounter];
+    colorMapCounter++;
+  }
+
+  return chalk.hex(colorMap[serviceName]).bold(serviceName);
+};
+
+const NAME_MAX_LENGTH = 18;
+
+function colorizeAndPad(name) {
+  return [colorize(name)]
+    .concat(times(NAME_MAX_LENGTH - name.length, () => ' '))
+    .join('');
+}
+
+const container = new DefaultServiceProvider(config).buildContainer();
+for (let key of Object.keys(container._services).sort()) {
+  const service = container._services[key];
+  const name = service.manager().name();
+  const depNames = service.manager().dependencies();
+  console.log(
+    `${colorizeAndPad(name)} : ${depNames.sort().map(colorize).join(', ') ||
+      'none'}`
+  );
+}
