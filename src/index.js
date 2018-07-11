9b4a5c7f95b52f044120369581b478df32e4e622
Lawrence Wang <l@lw.io>
2018-07-11 12:26:47 -0700

    change exports

--- src/index.js
+++ src/index.js
@@ -1,13 +1,15 @@
-import { default as Maker } from './Maker';
-module.exports = Maker;
-
+import Maker from './Maker';
 import { currencies } from './eth/Currency';
-for (let symbol in currencies) {
-  Maker[symbol] = currencies[symbol];
-}
-
 import LocalService from './core/LocalService';
 import PrivateService from './core/PrivateService';
 import PublicService from './core/PublicService';
 
-Maker.service = { LocalService, PrivateService, PublicService };
+for (let symbol in currencies) {
+  Maker[symbol] = currencies[symbol];
+}
+
+Maker.LocalService = LocalService;
+Maker.PrivateService = PrivateService;
+Maker.PublicService = PublicService;
+
+module.exports = Maker;
