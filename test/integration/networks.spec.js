fe67edb7f565dc0c908ee69a6b3e84d03881ad1b
Ethan Bennett <ebennett19@gmail.com>
2018-09-11 22:37:55 -0600

    Add basic integration tests for kovan/mainnet

--- test/integration/networks.spec.js
+++ test/integration/networks.spec.js
@@ -1,4 +1,40 @@
-test('mainnet test', () => {
-  console.log(process.env.NETWORK);
-  expect(1).toEqual(1);
+import Maker from '../../src/index';
+
+let maker, cdp, id;
+
+beforeAll(async () => {
+  if (!process.env.PRIVATE_KEY) {
+    throw new Error('Please set a private key to run integration tests.');
+  }
+
+  maker = Maker.create(process.env.NETWORK, {
+    privateKey: process.env.PRIVATE_KEY
+  });
+
+  await maker.authenticate();
+  cdp = await maker.openCdp();
 });
+
+test('can create Maker instance', () => {
+  expect(maker).toBeDefined();
+});
+
+test(
+  'can open a CDP',
+  async () => {
+    id = await cdp.getId();
+    expect(cdp).toBeDefined();
+    expect(typeof id).toEqual('number');
+  },
+  100000
+);
+
+test(
+  'can shut a CDP',
+  async () => {
+    await cdp.shut();
+    const info = await cdp.getInfo();
+    expect(info.lad).toBe('0x0000000000000000000000000000000000000000');
+  },
+  100000
+);
