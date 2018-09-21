bc99162be7c35d5a0f6768404f1e08e40c32a55a
Lawrence Wang <l@lw.io>
2018-09-20 19:05:14 -0700

    get testchain wrapper script working

---
+++ scripts/run-testchain.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+set -e
+
+CWD="${0%/*}"
+
+$CWD/set-polling-interval.sh
+$CWD/../testchain/scripts/launch.sh $@
--- scripts/run-testnet.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-set -e
-
-CWD=`dirname $0`
-
-$CWD/set-polling-interval.sh
-
-exec $CWD/../testchain/scripts/with-deployed-system $@
