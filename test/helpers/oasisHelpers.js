eb71c9a5c90523da1a14433f20847deccdc37f9c
Ethan Bennett <ebennett19@gmail.com>
2018-10-19 13:19:11 -0600

    Refactor oasis helpers

--- test/helpers/oasisHelpers.js
+++ test/helpers/oasisHelpers.js
@@ -3,90 +3,6 @@ import contracts from '../../contracts/contracts';
 import { DAI, WETH } from '../../src/eth/Currency';
 import { mineBlocks } from './transactionConfirmation';
 
-async function offer(
-  oasisExchangeService,
-  payAmount,
-  payTokenAddress,
-  buyAmount,
-  buyTokenAddress,
-  pos,
-  overrides
-) {
-  const oasisContract = oasisExchangeService
-    .get('smartContract')
-    .getContractByName(contracts.MAKER_OTC);
-
-  return oasisContract.offer(
-    payAmount,
-    payTokenAddress,
-    buyAmount,
-    buyTokenAddress,
-    pos,
-    overrides
-  );
-}
-
-function placeLimitOrder(oasisExchangeService, sellDai) {
-  let ethereumTokenService;
-  ethereumTokenService = oasisExchangeService.get('token');
-  const wethToken = ethereumTokenService.getToken(WETH);
-  const daiToken = ethereumTokenService.getToken(DAI);
-  return wethToken
-    .deposit('1')
-    .then(() => {
-      const oasisContract = oasisExchangeService
-        .get('smartContract')
-        .getContractByName(contracts.MAKER_OTC);
-      return wethToken.approveUnlimited(oasisContract.address);
-    })
-    .then(() => {
-      return wethToken.balanceOf(
-        oasisExchangeService.get('web3').currentAccount()
-      );
-    })
-    .then(() => {
-      return daiToken.balanceOf(
-        oasisExchangeService.get('web3').currentAccount()
-      );
-    })
-    .then(() => {
-      const wethAddress = wethToken.address();
-      const daiAddress = ethereumTokenService.getToken(DAI).address();
-      const overrideOptions = { gasLimit: 5500000 };
-      if (sellDai) {
-        return offer(
-          oasisExchangeService,
-          utils.parseEther('0.5'),
-          daiAddress,
-          utils.parseEther('2.0'),
-          wethAddress,
-          0,
-          overrideOptions
-        );
-      } else {
-        return offer(
-          oasisExchangeService,
-          utils.parseEther('0.5'),
-          wethAddress,
-          utils.parseEther('10.0'),
-          daiAddress,
-          1,
-          overrideOptions
-        );
-      }
-    })
-    .then(() => {
-      return wethToken.balanceOf(
-        oasisExchangeService.get('web3').currentAccount()
-      );
-    })
-    .then(() => {
-      return daiToken.balanceOf(
-        oasisExchangeService.get('web3').currentAccount()
-      );
-    });
-}
-
 export default async function createDaiAndPlaceLimitOrder(
   oasisExchangeService,
   sellDai = false
@@ -97,4 +13,60 @@ export default async function createDaiAndPlaceLimitOrder(
   await tx;
   await cdp.drawDai(1);
   return placeLimitOrder(oasisExchangeService, sellDai);
+}
+
+async function placeLimitOrder(oasisExchangeService, sellDai) {
+  let sellToken, buyToken, value, position;
+  const tokenService = oasisExchangeService.get('token');
+  const wethToken = tokenService.getToken(WETH);
+  const wethAddress = wethToken.address();
+  const daiAddress = tokenService.getToken(DAI).address();
+  const oasisAddress = oasisExchangeService
+    .get('smartContract')
+    .getContractByName(contracts.MAKER_OTC).address;
+
+  await wethToken.deposit('1');
+  await wethToken.approveUnlimited(oasisAddress);
+
+  if (sellDai) {
+    sellToken = daiAddress;
+    buyToken = wethAddress;
+    value = utils.parseEther('2.0');
+    position = 0;
+  } else {
+    sellToken = wethAddress;
+    buyToken = daiAddress;
+    value = utils.parseEther('10.0');
+    position = 1;
+  }
+
+  return offer(
+    oasisExchangeService,
+    utils.parseEther('0.5'),
+    sellToken,
+    value,
+    buyToken,
+    position
+  );
+}
+
+async function offer(
+  oasisExchangeService,
+  payAmount,
+  payTokenAddress,
+  buyAmount,
+  buyTokenAddress,
+  position
+) {
+  const oasisContract = oasisExchangeService
+    .get('smartContract')
+    .getContractByName(contracts.MAKER_OTC);
+
+  return oasisContract.offer(
+    payAmount,
+    payTokenAddress,
+    buyAmount,
+    buyTokenAddress,
+    position
+  );
 }
