8a935aafbda423a0423726e9247b5be7d2b04f27
ethanbennett <ebennett19@gmail.com>
2018-11-30 16:33:35 -0700

    Get token addresses for oasis params

--- src/exchanges/oasis/OasisDirectService.js
+++ src/exchanges/oasis/OasisDirectService.js
@@ -1,7 +1,66 @@
 import PrivateService from '../../core/PrivateService';
+import tokens from '../../../contracts/tokens';
+import { getCurrency } from '../../eth/Currency';
+import contracts from '../../../contracts/contracts';
+import { contractInfo } from '../../../contracts/networks';
 
 export default class OasisDirectService extends PrivateService {
   constructor(name = 'oasisDirect') {
-    super(name, ['proxy']);
+    super(name, ['proxy', 'smartContract', 'token', 'exchange']);
+  }
+
+  _valueForContract(amount, symbol) {
+    return getCurrency(amount, symbol).toEthersBigNumber('wei');
+  }
+
+  async getBalance(token) {
+    return await this.get('token')
+      .getToken(token)
+      .balanceOf(
+        this.get('smartContract')
+          .get('web3')
+          .currentAccount()
+      );
+  }
+
+  _getTokenAddress(symbol) {
+    switch (symbol) {
+      case 'MKR':
+        return this._contractInfo().MKR[1].address;
+      case 'WETH':
+        return this._contractInfo().WETH[0].address;
+      case 'PETH':
+        return this._contractInfo().PETH[0].address;
+      case 'DAI':
+        return this._contractInfo().DAI[0].address;
+    }
+  }
+
+  _contractInfo() {
+    return contractInfo(this.get('proxy')._network());
+  }
+
+  _otc() {
+    return this.get('smartContract').getContractByName(contracts.MAKER_OTC);
+  }
+
+  _oasisDirect() {
+    return this.get('smartContract').getContractByName(contracts.OASIS_PROXY);
+  }
+
+  async sellAllAmount(payTokenSymbol, buyTokenSymbol, payAmount) {
+    const payToken = this._getToken(payTokenSymbol);
+    const buyToken = this._getToken(buyTokenSymbol);
+    const amount = this._valueForContract(payAmount, payTokenSymbol);
+    const minBuyAmount = this._valueForContract(0, buyTokenSymbol);
+    // const otc =
+
+    return this._oasisDirect().sellAllAmount(
+      addresses.MAKER_OTC,
+      payToken,
+      amount,
+      buyToken,
+      minBuyAmount
+    );
   }
 }
--- test/exchanges/oasis/OasisDirectService.spec.js
+++ test/exchanges/oasis/OasisDirectService.spec.js
@@ -1,16 +1,40 @@
 import { buildTestService } from '../../helpers/serviceBuilders';
+import createDaiAndPlaceLimitOrder from '../../helpers/oasisHelpers';
 
-let service;
+let service, weth, dai, mkr;
 
 async function buildTestOasisDirectService() {
-  service = buildTestService('oasisDirect', { oasisDirect: true });
+  service = buildTestService('oasisDirect', {
+    oasisDirect: true,
+    exchange: 'OasisExchangeService'
+  });
   await service.manager().authenticate();
 }
 
 beforeEach(async () => {
   await buildTestOasisDirectService();
+  weth = service.get('token').getToken('WETH');
+  dai = service.get('token').getToken('DAI');
+  mkr = service.get('token').getToken('MKR');
 });
 
-test('startin er up', () => {
-  console.log(service);
+test('get token addresses', () => {
+  console.log(service._getTokenAddress('MKR'));
+  console.log(service._getTokenAddress('WETH'));
+  console.log(service._getTokenAddress('PETH'));
+  console.log(service._getTokenAddress('DAI'));
+});
+
+test('get token balances', async () => {
+  const balance = await service.getBalance(weth);
+  expect(balance.toString()).toEqual('0.00 WETH');
+});
+
+xtest('sell all amount', async () => {
+  await createDaiAndPlaceLimitOrder(service.get('exchange'));
+  try {
+    await service.sellAllAmount('MKR', 'DAI', 1);
+  } catch (err) {
+    console.error(err);
+  }
 });
