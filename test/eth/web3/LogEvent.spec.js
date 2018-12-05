88ec189238e26724f3040d98b1a4df04bd5bfa2c
sirromdev <sirromdev@gmail.com>
2018-12-05 17:51:05 +0000

    added event test for predicate resolve

--- test/eth/web3/LogEvent.spec.js
+++ test/eth/web3/LogEvent.spec.js
@@ -4,6 +4,7 @@ import getMatchingEvent, {
 } from '../../../src/eth/web3/LogEvent';
 import { buildTestService } from '../../helpers/serviceBuilders';
 import contracts from '../../../contracts/contracts';
+import util from 'util';
 
 describe('event subscriptions', () => {
   const event = isAnonymous => {
@@ -45,6 +46,40 @@ describe('event subscriptions', () => {
         }
       }
     };
+  };
+
+  const correctLogNewCup = () => {
+    return {
+      jsonrpc: '2.0',
+      method: 'eth_subscription',
+      params: {
+        subscription: '0x2',
+        result: {
+          data:
+            '0x0000000000000000000000000000000000000000000000000000000000000002',
+          topics: [
+            '0x89b8893b806db50897c8e2362c71571cfaeb9761ee40727f683f1793cda9df16',
+            '0x00000000000000000000000016fb96a5fa0427af0c8f7cf1eb4870231c8154b6'
+          ]
+        }
+      }
+    };
+  };
+
+  const expectedEvent = {
+    '0': '0x16Fb96a5fa0427Af0C8F7cF1eB4870231c8154B6',
+    '1': '0x0000000000000000000000000000000000000000000000000000000000000001',
+    __length__: 2,
+    cup: '0x0000000000000000000000000000000000000000000000000000000000000001',
+    lad: '0x16Fb96a5fa0427Af0C8F7cF1eB4870231c8154B6'
+  };
+
+  const correctEvent = {
+    '0': '0x16Fb96a5fa0427Af0C8F7cF1eB4870231c8154B6',
+    '1': '0x0000000000000000000000000000000000000000000000000000000000000002',
+    __length__: 2,
+    cup: '0x0000000000000000000000000000000000000000000000000000000000000002',
+    lad: '0x16Fb96a5fa0427Af0C8F7cF1eB4870231c8154B6'
   };
 
   function fireMockEvent(web3Service, eventObj, timeout, expects) {
@@ -112,19 +147,64 @@ describe('event subscriptions', () => {
       'LogNewCup'
     );
 
+    expect(util.inspect(eventPromise)).toMatch(/<pending>/);
     await fireMockEvent(web3Service, rawLogNewCup(), 100, () => {
       const { subscriptions } = web3Service._web3.eth._requestManager;
       expect(subscriptions['0x2'].name).toEqual('logs');
     });
 
-    const expectedEvent = {
-      '0': '0x16Fb96a5fa0427Af0C8F7cF1eB4870231c8154B6',
-      '1': '0x0000000000000000000000000000000000000000000000000000000000000001',
-      __length__: 2,
-      cup: '0x0000000000000000000000000000000000000000000000000000000000000001',
-      lad: '0x16Fb96a5fa0427Af0C8F7cF1eB4870231c8154B6'
-    };
+    // eventPromise callback will be triggered and will resolve itself
+    expect(util.inspect(eventPromise)).toMatch(/Result/);
     expect(eventPromise).resolves.toEqual(expectedEvent);
+
+    const { subscriptions } = web3Service._web3.eth._requestManager;
+    expect(subscriptions['0x2']).toBe(undefined);
+  });
+
+  test('mock subscription event with predicate', async () => {
+    const smartContractService = buildTestService('smartContract', {
+      smartContract: true,
+      useWebsockets: true
+    });
+    await smartContractService.manager().authenticate();
+    const web3Service = smartContractService.get('web3');
+
+    const contractAbi = smartContractService._getContractInfo(
+      contracts.SAI_TUB
+    );
+
+    const eventPromise = getMatchingEvent(
+      web3Service._web3,
+      contractAbi,
+      'LogNewCup',
+      log => {
+        const hexConvertAndPad = num => {
+          return web3Service._web3.utils.padLeft(
+            web3Service._web3.utils.numberToHex(num),
+            64
+          );
+        };
+        return log.cup === hexConvertAndPad(2);
+      }
+    );
+
+    expect(util.inspect(eventPromise)).toMatch(/<pending>/);
+    await fireMockEvent(web3Service, rawLogNewCup(), 100, () => {
+      const { subscriptions } = web3Service._web3.eth._requestManager;
+      expect(subscriptions['0x2'].name).toEqual('logs');
+    });
+
+    // eventPromise should not resolve as predicate is not satisfied
+    expect(util.inspect(eventPromise)).toMatch(/<pending>/);
+
+    await fireMockEvent(web3Service, correctLogNewCup(), 100, () => {
+      const { subscriptions } = web3Service._web3.eth._requestManager;
+      expect(subscriptions['0x2'].name).toEqual('logs');
+    });
+
+    // eventPromise should now be resolved as predicate is satisfied
+    expect(util.inspect(eventPromise)).toMatch(/Result/);
+    expect(eventPromise).resolves.toEqual(correctEvent);
 
     const { subscriptions } = web3Service._web3.eth._requestManager;
     expect(subscriptions['0x2']).toBe(undefined);
