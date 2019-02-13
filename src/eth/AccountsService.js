7d20ed9d64e1add128f4fa39b76c72ac4489c34d
ethanbennett <ebennett19@gmail.com>
2019-02-13 15:21:02 -0700

    Check for account before emitting account change event

--- src/eth/AccountsService.js
+++ src/eth/AccountsService.js
@@ -83,8 +83,9 @@ export default class AccountsService extends PublicService {
     if (!this._currentAccount || name === 'default') {
       this.useAccount(name);
     }
-
-    this.get('event').emit('accounts/ADD', { account: sanitizeAccount(account) });
+    if (this.hasAccount()) {
+      this.get('event').emit('accounts/ADD', { account: sanitizeAccount(account) });
+    }
 
     return account;
   }
@@ -116,8 +117,9 @@ export default class AccountsService extends PublicService {
     // add the provider at index 0 so that it takes precedence over RpcSource
     this._engine.addProvider(this.currentWallet(), 0);
     this._engine.start();
-
-    this.get('event').emit('accounts/CHANGE', { account: this.currentAccount() });
+    if (this.hasAccount()) {
+      this.get('event').emit('accounts/CHANGE', { account: this.currentAccount() });
+    }
   }
 
   _getAccountWithAddress(addr) {
