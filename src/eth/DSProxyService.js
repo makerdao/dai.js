f9a1abcef4be923ddf669f482481d7984fde9254
Lawrence Wang <l@lw.io>
2019-03-19 17:54:38 -0700

    add tracksTransactions to DSProxyService

--- src/eth/DSProxyService.js
+++ src/eth/DSProxyService.js
@@ -1,3 +1,4 @@
+import tracksTransactions from '../utils/tracksTransactions';
 import { PrivateService } from '@makerdao/services-core';
 import { dappHub } from '../../contracts/abis';
 import { Contract } from 'ethers';
@@ -34,7 +35,8 @@ export default class DSProxyService extends PrivateService {
       : this.getProxyAddress();
   }
 
-  async ensureProxy() {
+  @tracksTransactions
+  async ensureProxy({ promise }) {
     let proxy = await this.currentProxy();
     if (!proxy) {
       this.get('web3')
@@ -42,17 +44,18 @@ export default class DSProxyService extends PrivateService {
         .on('dsproxy/BUILD', obj => {
           proxy = obj.payload.address;
         });
-      await this.build();
+      await this.build({ promise });
     }
     return proxy;
   }
 
-  async build() {
+  @tracksTransactions
+  async build({ promise }) {
     const proxy = await this.currentProxy();
     if (proxy) {
       throw new Error('This account already has a proxy deployed at ' + proxy);
     }
-    const txo = await this._proxyRegistry().build();
+    const txo = await this._proxyRegistry().build({ promise });
     this._currentProxy = await this.getProxyAddress();
     this.get('web3')
       .get('event')
