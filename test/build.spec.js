9f3264cbe7dd0138c7d97e465c63b5800603953a
Tyler Sorensen <tyler@makerdao.com>
2019-04-23 12:30:12 -0400

    add test to draw dai from a cdp

--- test/build.spec.js
+++ test/build.spec.js
@@ -1,11 +1,25 @@
-const Maker = require('../dist/src');
+const Maker = require('../dist');
 
-async function createMaker(privateKey) {
-  return Maker.create('test', { privateKey, log: false });
+/*These tests are meant to catch errors in the Babel compilation
+process and/or package.json, since the other Jest tests run
+against the original source.*/
+
+async function createMaker() {
+  return Maker.create('test', { log: false });
 }
 
-test('can get a service from maker from the dist folder', async () => {
+test('can get a service from maker', async () => {
   const maker = await createMaker();
   const ethCdp = maker.service('cdp');
-  expect(!!ethCdp).toBe(true);
+  expect(ethCdp).toBeDefined();
+});
+
+test('can open a CD, lock eth and draw dai', async () => {
+  const maker = await createMaker();
+  const cdp = await maker.openCdp();
+  await cdp.lockEth(0.01);
+  const initialDebt = await cdp.getDebtValue();
+  await cdp.drawDai(0.1);
+  const currentDebt = await cdp.getDebtValue();
+  expect(currentDebt.toNumber()).toBeGreaterThan(initialDebt.toNumber());
 });
