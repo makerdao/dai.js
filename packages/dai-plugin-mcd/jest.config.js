5e42945dd181b3a57cfd1afe59bcd168cd69ee45
Andrea Law <me@andrealaw.co>
2019-06-27 14:53:15 +0900

    Fix for travis

--- packages/dai-plugin-mcd/jest.config.js
+++ packages/dai-plugin-mcd/jest.config.js
@@ -3,3 +3,22 @@ module.exports = {
   "testMatch": ["<rootDir>/test/*.spec.js"],
   "testPathIgnorePatterns": ["<rootDir>/node_modules/", "<rootDir>/dist/"]
 }
+
+module.exports = {
+  "rootDir": "./",
+  "coverageReporters": [
+    "json",
+    "lcov",
+    "text-summary"
+  ],
+  "collectCoverageFrom": [
+    "src/**/*.js"
+  ],
+  "globalSetup": "<rootDir>/test/setup-global.js",
+  "roots": [
+    "src",
+    "test"
+  ],
+  "setupFilesAfterEnv": ["<rootDir>/test/setup-test.js"],
+  "testPathIgnorePatterns": ["<rootDir>/node_modules/", "<rootDir>/test/integration/"]
+}
---
+++ packages/dai-plugin-mcd/test/helpers/ganache.js
@@ -0,0 +1,31 @@
+import fetch from 'node-fetch';
+
+const ganacheAddress = 'http://localhost:2000';
+let requestCount = 0;
+
+export function callGanache(method, params = []) {
+  return fetch(ganacheAddress, {
+    method: 'POST',
+    headers: {
+      Accept: 'application/json',
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({
+      jsonrpc: '2.0',
+      method,
+      params,
+      id: requestCount++
+    })
+  });
+}
+
+export async function takeSnapshot() {
+  const res = await callGanache('evm_snapshot');
+  const { result } = await res.json();
+  return parseInt(result, 16);
+}
+
+export async function restoreSnapshot(snapId) {
+  const res = await callGanache('evm_revert', [snapId]);
+  return (await res.json()).result;
+}
