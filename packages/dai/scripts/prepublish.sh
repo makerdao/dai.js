4a703ab1d3cc6111d4d88888d90d6271f9848019
Ethan <ebennett19@gmail.com>
2019-07-24 23:38:04 -0600

    Push tags/commits in scripts; prefix commits

--- packages/dai/scripts/prepublish.sh
+++ packages/dai/scripts/prepublish.sh
@@ -6,9 +6,28 @@ if [ $(basename $(pwd)) != "dai" ]; then
 fi
 
 yarn config set version-tag-prefix 'dai-v'
+yarn config set version-git-message "dai-v%s"
 yarn version
+
+PACKAGE_VERSION=$(cat package.json \
+  | grep version \
+  | head -1 \
+  | awk -F: '{ print $2 }' \
+  | sed 's/[",]//g')
+
+git push origin dev --no-verify
+git push origin "dai-v${PACKAGE_VERSION:1}"
 
 CWD=`dirname $0`
 rm -rf dist/*
-./build-backend.sh
-cd - >/dev/null
+babel contracts --out-dir ./dist/contracts
+babel contracts/addresses --out-dir ./dist/contracts/addresses
+babel src --out-dir ./dist/src
+
+copyfiles \
+  contracts/abis/* \
+  src/config/presets/* \
+  contracts/addresses/* \
+  dist
+
+cd dist/
