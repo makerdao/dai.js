e4948e6435459a095c7ecde7428c89350c909120
Ethan <ebennett19@gmail.com>
2019-07-29 23:40:29 -0600

    Refactor dai prepublish script

--- packages/dai/scripts/prepublish.sh
+++ packages/dai/scripts/prepublish.sh
@@ -9,13 +9,4 @@ yarn config set version-tag-prefix "dai-v"
 yarn config set version-git-message "dai-v%s"
 yarn version
 
-rm -rf dist/*
-babel contracts --out-dir ./dist/contracts
-babel contracts/addresses --out-dir ./dist/contracts/addresses
-babel src --out-dir ./dist/src
-
-copyfiles \
-  contracts/abis/* \
-  src/config/presets/* \
-  contracts/addresses/* \
-  dist
+./scripts/build-backend.sh
