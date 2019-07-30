bb19ec2677660bbc1ff6527bd8af83f202977830
Ethan <ebennett19@gmail.com>
2019-07-29 22:38:45 -0600

    Remove push and related variables from scripts

--- packages/dai-plugin-mcd/scripts/prepublish.sh
+++ packages/dai-plugin-mcd/scripts/prepublish.sh
@@ -9,16 +9,6 @@ yarn config set version-tag-prefix "dai-plugin-mcd-v"
 yarn config set version-git-message "dai-plugin-mcd-v%s"
 yarn version
 
-PACKAGE_VERSION=$(cat package.json \
-  | grep version \
-  | head -1 \
-  | awk -F: '{ print $2 }' \
-  | sed 's/[",]//g')
-
-git push origin dev --no-verify
-git push origin "dai-plugin-mcd-v${PACKAGE_VERSION:1}"
-
-CWD=`dirname $0`
 rm -rf dist/*
 cd ../..
 ./node_modules/.bin/babel --no-babelrc -d packages/dai-plugin-mcd/dist packages/dai-plugin-mcd/src
