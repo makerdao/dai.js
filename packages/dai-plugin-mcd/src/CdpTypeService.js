abd215ad40e38ade149b8b31691560384587e4cf
Lawrence Wang <l@lw.io>
2019-08-02 16:30:49 -0700

    CdpTypeService.useMulticall

--- packages/dai-plugin-mcd/src/CdpTypeService.js
+++ packages/dai-plugin-mcd/src/CdpTypeService.js
@@ -2,6 +2,10 @@ import { PublicService } from '@makerdao/services-core';
 import CdpType from './CdpType';
 import { ServiceRoles } from './constants';
 import assert from 'assert';
+import { toHex } from './utils/ethereum';
+import isEqual from 'lodash/isEqual';
+import set from 'lodash/set';
+import uniqWith from 'lodash/uniqWith';
 const { CDP_TYPE, SYSTEM_DATA, QUERY_API } = ServiceRoles;
 
 export default class CdpTypeService extends PublicService {
@@ -31,5 +35,87 @@ export default class CdpTypeService extends PublicService {
 
     assert(types.length <= 1, `${label} matches more than one cdp type`);
     assert(types.length > 0, `${label} matches no cdp type`);
+  }
+
+  useMulticall(watcher) {
+    this.cdpTypes.forEach(type => this.useMulticallForType(watcher, type));
+  }
+
+  useMulticallForType(watcher, cdpType) {
+    const { ilk } = cdpType;
+    const systemData = this.get(SYSTEM_DATA);
+    const scs = systemData.get('smartContract');
+    const schema = [
+      {
+        target: scs.getContractAddress('MCD_JUG'),
+        call: ['ilks(bytes32)(uint256,uint48)', toHex(ilk)],
+        returns: [[`ilk.${ilk}.jug.duty`], [`ilk.${ilk}.jug.rho`]]
+      },
+      {
+        target: scs.getContractAddress('MCD_VAT'),
+        call: [
+          'ilks(bytes32)(uint256,uint256,uint256,uint256,uint256)',
+          toHex(ilk)
+        ],
+        returns: [
+          [`ilk.${ilk}.vat.Art`],
+          [`ilk.${ilk}.vat.rate`],
+          [`ilk.${ilk}.vat.spot`],
+          [`ilk.${ilk}.vat.line`],
+          []
+        ]
+      },
+      {
+        target: scs.getContractAddress('MCD_SPOT'),
+        call: ['ilks(bytes32)(address,uint256)', toHex(ilk)],
+        returns: [[`ilk.${ilk}.spot.pip`], [`ilk.${ilk}.spot.mat`]]
+      },
+      {
+        target: scs.getContractAddress('MCD_CAT'),
+        call: ['ilks(bytes32)(address,uint256,uint256)', toHex(ilk)],
+        returns: [
+          [`ilk.${ilk}.cat.flip`],
+          [`ilk.${ilk}.cat.chop`],
+          [`ilk.${ilk}.cat.lump`]
+        ]
+      },
+      {
+        target: scs.getContractAddress(cdpType.currency.symbol),
+        call: [
+          'balanceOf(address)(uint256)',
+          systemData.adapterAddress(cdpType.ilk)
+        ],
+        returns: [[`ilk.${ilk}.adapterBalance`]]
+      },
+      {
+        target: scs.getContractAddress('MCD_SPOT'),
+        call: ['par()(uint256)'],
+        returns: [['system.par']]
+      }
+    ];
+
+    watcher.batch().subscribe(updates => {
+      const ilkUpdates = updates.filter(u => u.type.startsWith(`ilk.${ilk}.`));
+      for (let update of ilkUpdates) {
+        const [, , group, key] = update.type.split('.');
+
+        if (key) {
+          set(cdpType.cache, [group + 'Info', key], update.value);
+        } else {
+          switch (group) {
+            case 'adapterBalance':
+              cdpType.cache[group] = update.value;
+              break;
+            default:
+              throw new Error(`Don't know how to handle "${group}"`);
+          }
+        }
+      }
+
+      const parUpdate = updates.find(u => u.type === 'system.par');
+      if (parUpdate) cdpType.cache.par = parUpdate.value;
+    });
+
+    watcher.tap(model => uniqWith(model.concat(schema), isEqual));
   }
 }
---
+++ packages/dai-plugin-mcd/test/multicall.spec.js
@@ -0,0 +1,68 @@
+import { createWatcher } from '@makerdao/multicall';
+import { mcdMaker } from './helpers';
+import { ServiceRoles } from '../src/constants';
+import { ETH, USD } from '@makerdao/dai';
+
+describe('CdpType integration', () => {
+  let watcher, maker, scs;
+
+  beforeAll(async () => {
+    maker = await mcdMaker({
+      prefetch: false,
+      web3: { pollingInterval: 20 }
+    });
+
+    scs = maker.service('smartContract');
+    watcher = createWatcher([], {
+      rpcUrl: maker.service('web3').rpcUrl,
+      multicallAddress: scs.getContractAddress('MULTICALL'),
+      config: { interval: 50 }
+    });
+  });
+
+  test('populate CdpType cache', async () => {
+    const cts = maker.service(ServiceRoles.CDP_TYPE);
+    const ethA = cts.getCdpType(null, 'ETH-A');
+    expect(ethA.cache).toEqual({});
+
+    cts.useMulticall(watcher);
+    watcher.start();
+    await watcher.awaitInitialFetch();
+
+    // TODO don't hardcode all these values
+    expect(ethA.cache).toEqual({
+      adapterBalance: '0',
+      par: '1000000000000000000000000000',
+      jugInfo: {
+        duty: '1000000001547125957863212448',
+        rho: '1563241233'
+      },
+      vatInfo: {
+        Art: '0',
+        rate: '1000000000000000000000000000',
+        spot: '100000000000000000000000000000',
+        line: '100000000000000000000000000000000000000000000000000'
+      },
+      spotInfo: {
+        pip: expect.any(String),
+        mat: '1500000000000000000000000000'
+      },
+      catInfo: {
+        flip: expect.any(String),
+        chop: '1050000000000000000000000000',
+        lump: '1500000000000000000'
+      }
+    });
+
+    // testing these separately because they're EIP55 checksum addresses
+    expect(ethA.cache.spotInfo.pip.toLowerCase()).toEqual(
+      scs.getContractAddress('PIP_ETH')
+    );
+    expect(ethA.cache.catInfo.flip.toLowerCase()).toEqual(
+      scs.getContractAddress('MCD_FLIP_ETH_A')
+    );
+
+    const expectedPrice = USD(150).div(ETH(1));
+    expect(ethA.price.eq(expectedPrice)).toBeTruthy();
+  });
+});
