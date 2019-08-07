77b0970bc79a634ab6a1d52171efa691e184051a
Ethan <ebennett19@gmail.com>
2019-08-06 22:25:45 -0600

    Add mkr redeemer to migrations spec

--- packages/dai-plugin-migrations/test/MigrationService.spec.js
+++ packages/dai-plugin-migrations/test/MigrationService.spec.js
@@ -2,6 +2,7 @@ import { migrationMaker } from './helpers';
 import { ServiceRoles, Migrations } from '../src/constants';
 import SingleToMultiCdp from '../src/migrations/SingleToMultiCdp';
 import SDaiToMDai from '../src/migrations/SDaiToMDai';
+import MkrRedeemer from '../src/migrations/MkrRedeemer';
 
 let maker, service;
 
@@ -17,10 +18,11 @@ describe('Migration Service', () => {
     expect(ids).toEqual(
       expect.arrayContaining([
         Migrations.SINGLE_TO_MULTI_CDP,
-        Migrations.SDAI_TO_MDAI
+        Migrations.SDAI_TO_MDAI,
+        Migrations.MKR_REDEEMER
       ])
     );
-    expect(ids.length).toEqual(2);
+    expect(ids.length).toEqual(3);
   });
 
   test('getting each migration returns a valid migration', () => {
@@ -29,6 +31,9 @@ describe('Migration Service', () => {
     );
     expect(service.getMigration(Migrations.SDAI_TO_MDAI)).toBeInstanceOf(
       SDaiToMDai
+    );
+    expect(service.getMigration(Migrations.MKR_REDEEMER)).toBeInstanceOf(
+      MkrRedeemer
     );
   });
 
