e29a5279b301eecec804736e72ab6f4dc26a65d9
Lawrence Wang <l@lw.io>
2019-10-30 19:22:10 -0700

    add MigrationService.runAllChecks
    it only runs the two we care about for MCD launch at the moment
    
    also add caching to MigrationService so we don't repeatedly instantiate migrations

--- packages/dai-plugin-migrations/test/MigrationService.spec.js
+++ packages/dai-plugin-migrations/test/MigrationService.spec.js
@@ -1,4 +1,5 @@
 import { migrationMaker } from './helpers';
+import { mockCdpIds } from './helpers/mocks';
 import { ServiceRoles, Migrations } from '../src/constants';
 import SingleToMultiCdp from '../src/migrations/SingleToMultiCdp';
 import SDaiToMDai from '../src/migrations/SDaiToMDai';
@@ -9,50 +10,58 @@ import MkrRedeemer from '../src/migrations/MkrRedeemer';
 
 let maker, service;
 
-describe('Migration Service', () => {
-  beforeAll(async () => {
-    maker = await migrationMaker();
-    service = maker.service(ServiceRoles.MIGRATION);
-  });
+beforeAll(async () => {
+  maker = await migrationMaker();
+  service = maker.service(ServiceRoles.MIGRATION);
+});
 
-  test('can fetch a list of all migrations', () => {
-    const ids = service.getAllMigrationsIds();
+test('can fetch a list of all migrations', () => {
+  const ids = service.getAllMigrationsIds();
 
-    expect(ids).toEqual(
-      expect.arrayContaining([
-        Migrations.SINGLE_TO_MULTI_CDP,
-        Migrations.SDAI_TO_MDAI,
-        Migrations.GLOBAL_SETTLEMENT_SAVINGS_DAI,
-        Migrations.GLOBAL_SETTLEMENT_COLLATERAL_CLAIMS,
-        Migrations.GLOBAL_SETTLEMENT_DAI_REDEEMER,
-        Migrations.MKR_REDEEMER
-      ])
-    );
-    expect(ids.length).toEqual(6);
-  });
+  expect(ids).toEqual(
+    expect.arrayContaining([
+      Migrations.SINGLE_TO_MULTI_CDP,
+      Migrations.SDAI_TO_MDAI,
+      Migrations.GLOBAL_SETTLEMENT_SAVINGS_DAI,
+      Migrations.GLOBAL_SETTLEMENT_COLLATERAL_CLAIMS,
+      Migrations.GLOBAL_SETTLEMENT_DAI_REDEEMER,
+      Migrations.MKR_REDEEMER
+    ])
+  );
+  expect(ids.length).toEqual(6);
+});
 
-  test('getting each migration returns a valid migration', () => {
-    expect(service.getMigration(Migrations.SINGLE_TO_MULTI_CDP)).toBeInstanceOf(
-      SingleToMultiCdp
-    );
-    expect(service.getMigration(Migrations.SDAI_TO_MDAI)).toBeInstanceOf(
-      SDaiToMDai
-    );
-    expect(
-      service.getMigration(Migrations.GLOBAL_SETTLEMENT_SAVINGS_DAI)
-    ).toBeInstanceOf(GlobalSettlementSavingsDai);
-    expect(
-      service.getMigration(Migrations.GLOBAL_SETTLEMENT_COLLATERAL_CLAIMS)
-    ).toBeInstanceOf(GlobalSettlementCollateralClaims);
-    expect(
-      service.getMigration(Migrations.GLOBAL_SETTLEMENT_DAI_REDEEMER)
-    ).toBeInstanceOf(GlobalSettlementDaiRedeemer);
-    expect(service.getMigration(Migrations.MKR_REDEEMER)).toBeInstanceOf(
-      MkrRedeemer
-    );
-  });
+test('getting each migration returns a valid migration', () => {
+  expect(service.getMigration(Migrations.SINGLE_TO_MULTI_CDP)).toBeInstanceOf(
+    SingleToMultiCdp
+  );
+  expect(service.getMigration(Migrations.SDAI_TO_MDAI)).toBeInstanceOf(
+    SDaiToMDai
+  );
+  expect(
+    service.getMigration(Migrations.GLOBAL_SETTLEMENT_SAVINGS_DAI)
+  ).toBeInstanceOf(GlobalSettlementSavingsDai);
+  expect(
+    service.getMigration(Migrations.GLOBAL_SETTLEMENT_COLLATERAL_CLAIMS)
+  ).toBeInstanceOf(GlobalSettlementCollateralClaims);
+  expect(
+    service.getMigration(Migrations.GLOBAL_SETTLEMENT_DAI_REDEEMER)
+  ).toBeInstanceOf(GlobalSettlementDaiRedeemer);
+  expect(service.getMigration(Migrations.MKR_REDEEMER)).toBeInstanceOf(
+    MkrRedeemer
+  );
+});
 
-  test('getting a non-existent migration returns undefined', () => {
-    expect(service.getMigration('non-existent')).toBeUndefined();
+test('getting a non-existent migration returns undefined', () => {
+  expect(service.getMigration('non-existent')).toBeUndefined();
+});
+
+test('runAllChecks', async () => {
+  mockCdpIds(maker);
+  const result = await service.runAllChecks();
+  expect(result).toEqual({
+    [Migrations.SDAI_TO_MDAI]: expect.anything(),
+    [Migrations.SINGLE_TO_MULTI_CDP]: {}
   });
+  expect(result[Migrations.SDAI_TO_MDAI].eq(0)).toBeTruthy();
 });
--- packages/dai-plugin-migrations/test/migrations/SingleToMultiCdp.spec.js
+++ packages/dai-plugin-migrations/test/migrations/SingleToMultiCdp.spec.js
@@ -1,24 +1,10 @@
-import { migrationMaker } from '../helpers';
+import { migrationMaker } from './helpers';
+import { mockCdpIds } from './helpers/mocks';
 import { ServiceRoles, Migrations } from '../../src/constants';
 import { takeSnapshot, restoreSnapshot } from '@makerdao/test-helpers';
 import { SAI } from '../../src/index';
 
 let maker, migration, snapshotData;
-
-async function mockCdpIds({ forAccount, forProxy } = {}) {
-  const currentAddress = maker.currentAddress();
-  const currentProxy = await maker.currentProxy();
-
-  maker.service('cdp').getCdpIds = jest.fn().mockImplementation(addr => {
-    if (addr === currentAddress) {
-      return forAccount || [];
-    } else if (addr === currentProxy) {
-      return forProxy || [];
-    } else {
-      return [];
-    }
-  });
-}
 
 async function drawSaiAndMigrateToDai(drawAmount) {
   const cdp = await maker.openCdp();
@@ -49,13 +35,13 @@ describe('SCD to MCD CDP Migration', () => {
   });
 
   test('if there are no cdps, return false', async () => {
-    await mockCdpIds();
+    await mockCdpIds(maker);
 
     expect(await migration.check()).toMatchObject({});
   });
 
   test('if there are cdps owned by a proxy, but no cdps owned by the account, return true', async () => {
-    await mockCdpIds({ forProxy: [{ id: '123' }] });
+    await mockCdpIds(maker, { forProxy: [{ id: '123' }] });
     expect(await migration.check()).toMatchObject({
       [await maker.currentProxy()]: [{ id: '123' }],
       [maker.currentAddress()]: []
@@ -63,7 +49,7 @@ describe('SCD to MCD CDP Migration', () => {
   });
 
   test('if there are cdps owned by the account, but no cdps owned by a proxy, return true', async () => {
-    await mockCdpIds({ forAccount: [{ id: '123' }] });
+    await mockCdpIds(maker, { forAccount: [{ id: '123' }] });
     expect(await migration.check()).toMatchObject({
       [await maker.currentProxy()]: [],
       [maker.currentAddress()]: [{ id: '123' }]
@@ -71,7 +57,7 @@ describe('SCD to MCD CDP Migration', () => {
   });
 
   test('if there are both cdps owned by the account and proxy, return true', async () => {
-    await mockCdpIds({
+    await mockCdpIds(maker, {
       forAccount: [{ id: '123' }],
       forProxy: [{ id: '234' }]
     });
