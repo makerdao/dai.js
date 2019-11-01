7c48b0ca546488c626dd5ded0ec0c661c2d8b391
Lawrence Wang <l@lw.io>
2019-10-31 20:05:06 -0700

    fix indentation

--- packages/dai-plugin-migrations/test/migrations/SingleToMultiCdp.spec.js
+++ packages/dai-plugin-migrations/test/migrations/SingleToMultiCdp.spec.js
@@ -43,50 +43,48 @@ describe('SCD to MCD CDP Migration', () => {
     await restoreSnapshot(snapshotData, maker);
   });
 
-  describe('checks', () => {
-    test('if there are no cdps, return false', async () => {
-      await mockCdpIds(maker);
+  test('if there are no cdps, return false', async () => {
+    await mockCdpIds(maker);
 
-      expect(await migration.check()).toMatchObject({});
-    });
+    expect(await migration.check()).toMatchObject({});
+  });
 
-    test('if there are cdps owned by a proxy, but no cdps owned by the account, return true', async () => {
-      await mockCdpIds(maker, { forProxy: [{ id: '123' }] });
-      expect(await migration.check()).toMatchObject({
-        [await maker.currentProxy()]: [{ id: '123' }],
-        [maker.currentAddress()]: []
-      });
+  test('if there are cdps owned by a proxy, but no cdps owned by the account, return true', async () => {
+    await mockCdpIds(maker, { forProxy: [{ id: '123' }] });
+    expect(await migration.check()).toMatchObject({
+      [await maker.currentProxy()]: [{ id: '123' }],
+      [maker.currentAddress()]: []
     });
+  });
 
-    test('if there are cdps owned by the account, but no cdps owned by a proxy, return true', async () => {
-      await mockCdpIds(maker, { forAccount: [{ id: '123' }] });
-      expect(await migration.check()).toMatchObject({
-        [await maker.currentProxy()]: [],
-        [maker.currentAddress()]: [{ id: '123' }]
-      });
+  test('if there are cdps owned by the account, but no cdps owned by a proxy, return true', async () => {
+    await mockCdpIds(maker, { forAccount: [{ id: '123' }] });
+    expect(await migration.check()).toMatchObject({
+      [await maker.currentProxy()]: [],
+      [maker.currentAddress()]: [{ id: '123' }]
     });
+  });
 
-    test('if there are both cdps owned by the account and proxy, return true', async () => {
-      await mockCdpIds(maker, {
-        forAccount: [{ id: '123' }],
-        forProxy: [{ id: '234' }]
-      });
-      expect(await migration.check()).toMatchObject({
-        [await maker.currentProxy()]: [{ id: '234' }],
-        [maker.currentAddress()]: [{ id: '123' }]
-      });
+  test('if there are both cdps owned by the account and proxy, return true', async () => {
+    await mockCdpIds(maker, {
+      forAccount: [{ id: '123' }],
+      forProxy: [{ id: '234' }]
     });
+    expect(await migration.check()).toMatchObject({
+      [await maker.currentProxy()]: [{ id: '234' }],
+      [maker.currentAddress()]: [{ id: '123' }]
+    });
+  });
 
-    test('if there is no sai locked in the mcd migration cdp, return 0', async () => {
-      const saiLiquidity = await migration.migrationSaiAvailable();
-      expect(saiLiquidity.toFixed('wei')).toBe('0');
-    });
+  test('if there is no sai locked in the mcd migration cdp, return 0', async () => {
+    const saiLiquidity = await migration.migrationSaiAvailable();
+    expect(saiLiquidity.toFixed('wei')).toBe('0');
+  });
 
-    test('if there is sai locked in the mcd migration cdp, return the amount that is there', async () => {
-      await drawSaiAndMigrateToDai(10); // lock 10 sai into the mcd migration cdp
-      const saiLiquidity = await migration.migrationSaiAvailable();
-      expect(saiLiquidity.toFixed('wei')).toBe('9999999999999999999');
-    });
+  test('if there is sai locked in the mcd migration cdp, return the amount that is there', async () => {
+    await drawSaiAndMigrateToDai(10); // lock 10 sai into the mcd migration cdp
+    const saiLiquidity = await migration.migrationSaiAvailable();
+    expect(saiLiquidity.toFixed('wei')).toBe('9999999999999999999');
   });
 
   describe('migrations', () => {
