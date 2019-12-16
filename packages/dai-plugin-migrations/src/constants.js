ff00507910e824440c22f937e9dfaf120a3e68df
Josh Levine <joshuapark95@gmail.com>
2019-12-16 17:15:41 -0500

    account for chief mig in mig service spec

--- packages/dai-plugin-migrations/src/constants.js
+++ packages/dai-plugin-migrations/src/constants.js
@@ -12,8 +12,7 @@ export const Migrations = {
   GLOBAL_SETTLEMENT_COLLATERAL_CLAIMS: 'global-settlement-collateral-claims',
   GLOBAL_SETTLEMENT_DAI_REDEEMER: 'global-settlement-dai-redeemer',
   MKR_REDEEMER: 'mkr-redeemer',
-  CHIEF_MIGRATE: 'chief-migrate',
-  VOTE_PROXY: 'vote-proxy'
+  CHIEF_MIGRATE: 'chief-migrate'
 };
 
 export const WAD = new BigNumber('1e18');
