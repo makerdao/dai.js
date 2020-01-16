ec58542a0de334f61d142cd671a01eb9585771b9
sirromdev <sirromdev@gmail.com>
2020-01-16 19:53:57 +0000

    Changed constant names

--- packages/dai-plugin-mcd/src/schema.js
+++ packages/dai-plugin-mcd/src/schema.js
@@ -1,9 +1,9 @@
-import { createCurrencyRatio } from '@makerdao/currency';
+import { createCurrency, createCurrencyRatio } from '@makerdao/currency';
 import { toHex, fromWei, fromRay, fromRad } from './utils';
 import BigNumber from 'bignumber.js';
 import { USD, ETH, BAT, MDAI } from '..';
 
-export const proxyAddress = 'proxyAddress';
+export const PROXY_ADDRESS = 'proxyAddress';
 
 export const proxies = {
   generate: address => ({
@@ -11,16 +11,16 @@ export const proxies = {
     contractName: 'PROXY_REGISTRY',
     call: ['proxies(address)(address)', address]
   }),
-  returns: [[proxyAddress]]
+  returns: [[PROXY_ADDRESS]]
 };
 
-export const totalEncumberedDebt = 'totalEncumberedDebt';
-export const debtScalingFactor = 'debtScalingFactor';
-export const priceWithSafetyMargin = 'priceWithSafetyMargin';
-export const debtCeiling = 'debtCeiling';
-export const urnDebtFloor = 'urnDebtFloor';
+export const TOTAL_ENCUMBERED_DEBT = 'totalEncumberedDebt';
+export const DEBT_SCALING_FACTOR = 'debtScalingFactor';
+export const PRICE_WITH_SAFETY_MARGIN = 'priceWithSafetyMargin';
+export const DEBT_CEILING = 'debtCeiling';
+export const URN_DEBT_FLOOR = 'urnDebtFloor';
 
-export const ilks = {
+export const vatIlks = {
   generate: ilkName => ({
     id: `MCD_VAT.ilks(${ilkName})`,
     contractName: 'MCD_VAT',
@@ -30,47 +30,89 @@ export const ilks = {
     ]
   }),
   returns: [
-    [totalEncumberedDebt, BigNumber],
-    [debtScalingFactor, fromRay],
-    [priceWithSafetyMargin, fromRay],
-    [debtCeiling, fromRad],
-    [urnDebtFloor, fromRad]
+    [TOTAL_ENCUMBERED_DEBT, BigNumber],
+    [DEBT_SCALING_FACTOR, fromRay],
+    [PRICE_WITH_SAFETY_MARGIN, fromRay],
+    [DEBT_CEILING, fromRad],
+    [URN_DEBT_FLOOR, fromRad]
   ]
 };
 
-export const totalDaiSupply = 'totalDaiSupply';
+export const TOTAL_DAI_SUPPLY = 'totalDaiSupply';
 export const debt = {
   generate: () => ({
     id: `VAT.debt()`,
     contractName: 'MCD_VAT',
     call: ['debt()(uint256)']
   }),
-  returns: [[totalDaiSupply, MDAI.rad]]
+  returns: [[TOTAL_DAI_SUPPLY, MDAI.rad]]
 };
 
+export const PRICE_FEED_ADDRESS = 'priceFeedAddress';
+export const RAW_LIQUIDATION_RATIO = 'rawLiquidationRatio';
+
+export const spotIlks = {
+  generate: ilkName => ({
+    id: `MCD_SPOT.ilks(${ilkName})`,
+    contractName: 'MCD_SPOT',
+    call: ['ilks(bytes32)(address,uint256)', toHex(ilkName)]
+  }),
+  returns: [PRICE_FEED_ADDRESS, [RAW_LIQUIDATION_RATIO, fromRay]]
+};
+
+export const LIQUIDATION_RATIO = 'liquidationRatio';
+export const liquidationRatio = {
+  // The liquidation ratio value is the minimum dollar amount of collateral in
+  // terms of a single dollar unit amount of debt
+  //
+  // In plain english, it is the ratio of the dollar amount of ETH in terms of
+  // the dollar amount of dai
+  generate: ilkName => ({
+    dependencies: () => [[RAW_LIQUIDATION_RATIO, ilkName]],
+    computed: liqRatio =>
+      createCurrencyRatio(
+        createCurrency(`(${ilkName.split('-')[0]}/USD)`),
+        createCurrency(`(${MDAI.symbol}/USD)`)
+      )(liqRatio)
+  })
+};
+
+export const RATIO_DAI_USD = 'ratioDaiUsd';
+export const spotPar = {
+  generate: () => ({
+    id: 'MCD_SPOT.par()',
+    contractName: 'MCD_SPOT',
+    call: ['par()(uint256)']
+  }),
+  returns: [[RATIO_DAI_USD, v => createCurrencyRatio(MDAI, USD)(fromRay(v))]]
+};
+
+export const ILK_PRICES = 'ilkPrices';
 export const ilkPrices = {
   generate: ilkNames => ({
     // Dynamically generated dependencies
     dependencies: () => [
-      ['refPerDai'],
+      [RATIO_DAI_USD],
       ...ilkNames.reduce(
         (acc, ilk) => [
           ...acc,
-          ['priceWithSafetyMargin', ilk],
-          ['liquidationRatio', ilk]
+          [PRICE_WITH_SAFETY_MARGIN, ilk],
+          [LIQUIDATION_RATIO, ilk]
         ],
         []
       )
     ],
-    computed: (refPerDai, ...results) =>
+    computed: (ratioDaiUsd, ...results) =>
       results.reduce((acc, r, i) => {
         if (i % 2 === 0) {
-          const currency = ETH;
           const priceWithSafetyMargin = results[i];
           const liquidationRatio = results[i + 1];
+          const currency = createCurrency(
+            liquidationRatio.numerator.symbol.substring(1, 4)
+          );
           const ratio = createCurrencyRatio(USD, currency);
           const price = priceWithSafetyMargin
-            .times(refPerDai)
+            .times(ratioDaiUsd.toNumber())
             .times(liquidationRatio.toNumber());
           return [...acc, ratio(price)];
         }
@@ -79,38 +121,12 @@ export const ilkPrices = {
   })
 };
 
-export const priceFeedAddress = 'priceFeedAddress';
-export const liquidationRatio = 'liquidationRatio';
-export const spotIlks = {
-  generate: ilkName => ({
-    id: `MCD_SPOT.ilks(${ilkName})`,
-    contractName: 'MCD_SPOT',
-    call: ['ilks(bytes32)(address,uint256)', toHex(ilkName)]
-  }),
-  returns: [
-    priceFeedAddress,
-    [
-      liquidationRatio,
-      v => createCurrencyRatio(USD, MDAI)(BigNumber(fromRay(v)))
-    ]
-  ]
-};
-
-export const refPerDai = 'refPerDai';
-export const spotPar = {
-  generate: () => ({
-    id: 'MCD_SPOT.par()',
-    contractName: 'MCD_SPOT',
-    call: ['par()(uint256)']
-  }),
-  returns: [[refPerDai, fromRay]]
-};
-
 export default {
-  ilks,
+  vatIlks,
   proxies,
   debt,
   spotIlks,
   spotPar,
-  ilkPrices
+  ilkPrices,
+  liquidationRatio
 };
--- packages/dai-plugin-mcd/test/schema.spec.js
+++ packages/dai-plugin-mcd/test/schema.spec.js
@@ -1,5 +1,5 @@
 import { mcdMaker, setupCollateral } from './helpers';
-import { ETH, BAT, MDAI } from '../src';
+import { ETH, BAT, MDAI, USD } from '../src';
 import {
   toHex,
   fromRad,
@@ -7,15 +7,6 @@ import {
   isBigNumber,
   isValidAddressString
 } from '../src/utils';
-import schemas, {
-  totalEncumberedDebt,
-  debtScalingFactor,
-  priceWithSafetyMargin,
-  debtCeiling,
-  urnDebtFloor,
-  proxyAddress,
-  totalDaiSupply
-} from '../src/schema';
 import { ServiceRoles } from '../src/constants';
 import BigNumber from 'bignumber.js';
 let mcall,
@@ -28,6 +19,22 @@ let mcall,
   ethAInfo,
   batAInfo,
   vault;
+import { createCurrencyRatio } from '@makerdao/currency';
+
+import schemas, {
+  TOTAL_ENCUMBERED_DEBT,
+  DEBT_SCALING_FACTOR,
+  PRICE_WITH_SAFETY_MARGIN,
+  DEBT_CEILING,
+  URN_DEBT_FLOOR,
+  PROXY_ADDRESS,
+  TOTAL_DAI_SUPPLY,
+  PRICE_FEED_ADDRESS,
+  RAW_LIQUIDATION_RATIO,
+  RATIO_DAI_USD,
+  LIQUIDATION_RATIO,
+  ILK_PRICES
+} from '../src/schema';
 
 const ETH_A_COLLATERAL_AMOUNT = ETH(1);
 const ETH_A_DEBT_AMOUNT = MDAI(1);
@@ -80,7 +87,7 @@ beforeAll(async () => {
   batAInfo = await cdpTypeService.getCdpType(BAT, 'BAT-A').ilkInfo();
 });
 
-test(totalEncumberedDebt, async () => {
+test(TOTAL_ENCUMBERED_DEBT, async () => {
   // TODO Define hardcoded rates for given ilks outside of the system and test
   // against those rather than data extracted from the chain
   const { rate: ethARate } = ethAInfo;
@@ -89,8 +96,8 @@ test(totalEncumberedDebt, async () => {
   const ethADebtAmount = ETH_A_DEBT_AMOUNT._amount;
   const batADebtAmount = BAT_A_DEBT_AMOUNT._amount;
 
-  const ethAEncumberedDebt = await maker.latest(totalEncumberedDebt, 'ETH-A');
-  const batAEncumberedDebt = await maker.latest(totalEncumberedDebt, 'BAT-A');
+  const ethAEncumberedDebt = await maker.latest(TOTAL_ENCUMBERED_DEBT, 'ETH-A');
+  const batAEncumberedDebt = await maker.latest(TOTAL_ENCUMBERED_DEBT, 'BAT-A');
 
   expect(isBigNumber(ethAEncumberedDebt)).toEqual(true);
   expect(isBigNumber(batAEncumberedDebt)).toEqual(true);
@@ -110,12 +117,18 @@ test(totalEncumberedDebt, async () => {
   );
 });
 
-test(debtScalingFactor, async () => {
+test(DEBT_SCALING_FACTOR, async () => {
   const { rate: ethARate } = ethAInfo;
   const { rate: batARate } = batAInfo;
 
-  const ethADebtScalingFactor = await maker.latest(debtScalingFactor, 'ETH-A');
-  const batADebtScalingFactor = await maker.latest(debtScalingFactor, 'BAT-A');
+  const ethADebtScalingFactor = await maker.latest(
+    DEBT_SCALING_FACTOR,
+    'ETH-A'
+  );
+  const batADebtScalingFactor = await maker.latest(
+    DEBT_SCALING_FACTOR,
+    'BAT-A'
+  );
 
   expect(isBigNumber(ethADebtScalingFactor)).toEqual(true);
   expect(isBigNumber(batADebtScalingFactor)).toEqual(true);
@@ -124,13 +137,13 @@ test(debtScalingFactor, async () => {
   expect(batADebtScalingFactor).toEqual(fromRay(batARate));
 });
 
-test(priceWithSafetyMargin, async () => {
+test(PRICE_WITH_SAFETY_MARGIN, async () => {
   const ethAPriceWithSafetyMargin = await maker.latest(
-    priceWithSafetyMargin,
+    PRICE_WITH_SAFETY_MARGIN,
     'ETH-A'
   );
   const batAPriceWithSafetyMargin = await maker.latest(
-    priceWithSafetyMargin,
+    PRICE_WITH_SAFETY_MARGIN,
     'BAT-A'
   );
 
@@ -146,9 +159,9 @@ test(priceWithSafetyMargin, async () => {
   expect(batAPriceWithSafetyMargin).toEqual(batACalculatedMargin);
 });
 
-test(debtCeiling, async () => {
-  const ethADebtCeiling = await maker.latest(debtCeiling, 'ETH-A');
-  const batADebtCeiling = await maker.latest(debtCeiling, 'BAT-A');
+test(DEBT_CEILING, async () => {
+  const ethADebtCeiling = await maker.latest(DEBT_CEILING, 'ETH-A');
+  const batADebtCeiling = await maker.latest(DEBT_CEILING, 'BAT-A');
 
   expect(isBigNumber(ethADebtCeiling)).toEqual(true);
   expect(isBigNumber(batADebtCeiling)).toEqual(true);
@@ -157,9 +170,9 @@ test(debtCeiling, async () => {
   expect(batADebtCeiling).toEqual(BigNumber('5000'));
 });
 
-test(urnDebtFloor, async () => {
-  const ethAUrnDebtFloor = await maker.latest(urnDebtFloor, 'ETH-A');
-  const batAUrnDebtFloor = await maker.latest(urnDebtFloor, 'BAT-A');
+test(URN_DEBT_FLOOR, async () => {
+  const ethAUrnDebtFloor = await maker.latest(URN_DEBT_FLOOR, 'ETH-A');
+  const batAUrnDebtFloor = await maker.latest(URN_DEBT_FLOOR, 'BAT-A');
 
   expect(isBigNumber(ethAUrnDebtFloor)).toEqual(true);
   expect(isBigNumber(batAUrnDebtFloor)).toEqual(true);
@@ -168,7 +181,7 @@ test(urnDebtFloor, async () => {
   expect(batAUrnDebtFloor).toEqual(BigNumber('0'));
 });
 
-test(totalDaiSupply, async () => {
+test(TOTAL_DAI_SUPPLY, async () => {
   const { Art: ethAArt, rate: ethARate } = ethAInfo;
   const { Art: batAArt, rate: batARate } = batAInfo;
 
@@ -176,20 +189,67 @@ test(totalDaiSupply, async () => {
   const batADaiGenerated = MDAI.rad(BigNumber(batAArt).times(batARate));
   const sumOfDaiGeneratedFromIlks = ethADaiGenerated.plus(batADaiGenerated);
 
-  const totalDaiAmount = await maker.latest(totalDaiSupply);
+  const totalDaiAmount = await maker.latest(TOTAL_DAI_SUPPLY);
 
   expect(totalDaiAmount.symbol).toEqual('MDAI');
   expect(totalDaiAmount.isEqual(sumOfDaiGeneratedFromIlks)).toEqual(true);
 });
 
-test(proxyAddress, async () => {
-  const proxy = await maker.latest(proxyAddress, address);
-  expect(isValidAddressString(proxy)).toEqual(true);
-  expect(proxy).toEqual('0xC21eDD3d1Ba1bCCD67008B680b362ce6F344DaB3');
+test(PROXY_ADDRESS, async () => {
+  const proxyAddress = await maker.latest(PROXY_ADDRESS, address);
+
+  expect(isValidAddressString(proxyAddress)).toEqual(true);
+  expect(proxyAddress).toEqual('0xC21eDD3d1Ba1bCCD67008B680b362ce6F344DaB3');
 });
 
-test.skip('ilkPrices', async () => {
-  const [ethAPrice, ethBPrice, batAPrice] = await maker.latest('ilkPrices', [
+test(PRICE_FEED_ADDRESS, async () => {
+  const ethAPriceFeedAddress = await maker.latest(PRICE_FEED_ADDRESS, 'ETH-A');
+  const batAPriceFeedAddress = await maker.latest(PRICE_FEED_ADDRESS, 'BAT-A');
+
+  expect(isValidAddressString(ethAPriceFeedAddress)).toEqual(true);
+  expect(isValidAddressString(batAPriceFeedAddress)).toEqual(true);
+
+  expect(ethAPriceFeedAddress).toEqual(
+    '0xaF14E6E871f81BB92f151AfF1bB80936Aa06C6D6'
+  );
+  expect(batAPriceFeedAddress).toEqual(
+    '0x7eD0d0255153050e9623FfECEeE49a1020503CA3'
+  );
+});
+
+test(RAW_LIQUIDATION_RATIO, async () => {
+  const ethARawLiquidationRatio = await maker.latest(
+    RAW_LIQUIDATION_RATIO,
+    'ETH-A'
+  );
+  const batARawLiquidationRatio = await maker.latest(
+    RAW_LIQUIDATION_RATIO,
+    'BAT-A'
+  );
+
+  expect(ethARawLiquidationRatio).toEqual(BigNumber('1.5'));
+  expect(batARawLiquidationRatio).toEqual(BigNumber('2.0'));
+});
+
+test(LIQUIDATION_RATIO, async () => {
+  const ethALiquidationRatio = await maker.latest(LIQUIDATION_RATIO, 'ETH-A');
+  const batALiquidationRatio = await maker.latest(LIQUIDATION_RATIO, 'BAT-A');
+
+  expect(ethALiquidationRatio.symbol).toEqual('(ETH/USD)/(MDAI/USD)');
+  expect(batALiquidationRatio.symbol).toEqual('(BAT/USD)/(MDAI/USD)');
+
+  expect(ethALiquidationRatio.toNumber()).toEqual(1.5);
+  expect(batALiquidationRatio.toNumber()).toEqual(2.0);
+});
+
+test(RATIO_DAI_USD, async () => {
+  const ratio = await maker.latest(RATIO_DAI_USD);
+  expect(ratio.symbol).toEqual('MDAI/USD');
+  expect(ratio.toNumber()).toEqual(1);
+});
+
+test(ILK_PRICES, async () => {
+  const [ethAPrice, ethBPrice, batAPrice] = await maker.latest(ILK_PRICES, [
     'ETH-A',
     'ETH-B',
     'BAT-A'
