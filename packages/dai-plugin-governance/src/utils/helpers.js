66795c014a025d3ba9777f9f9144ebf4d7500270
Phil Bain <bain.pmcg@gmail.com>
2020-01-17 15:01:41 +0100

    Revert "CDP-733 schemas for duty and rho"
    This reverts commit d5402d61a057a2ee9e5de063189263b4ad011b35.

--- packages/dai-plugin-governance/src/utils/helpers.js
+++ packages/dai-plugin-governance/src/utils/helpers.js
@@ -1,5 +1,10 @@
 import { createGetCurrency } from '@makerdao/currency';
-import { MKR, STAGING_MAINNET_URL, KOVAN_URL, MAINNET_URL } from './constants';
+import {
+  MKR,
+  STAGING_MAINNET_URL,
+  KOVAN_URL,
+  MAINNET_URL
+} from './constants';
 
 /**
  * @desc get network name
