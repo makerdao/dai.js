8a241b12aa4387be0b9182314c03888b4f059677
Michael Elliot <michael@digital.com.au>
2020-01-22 20:31:17 +0800

    Add initial type definitions for vaultById and VaultResult

--- packages/dai-plugin-mcd/typings/multicall.d.ts
+++ packages/dai-plugin-mcd/typings/multicall.d.ts
@@ -1,4 +1,47 @@
 declare global {
+  interface Currency {}
+  interface CurrencyRatio {}
+  interface VaultResult {
+    /**
+     * The urn address of this vault.
+     *
+     * Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+     * tempor incididunt ut labore et dolore magna aliqua.
+     *
+     * ```ts
+     * console.log('test code');
+     * ```
+     */
+    urn: string;
+
+    /**
+     * The price of this vault's ilk.
+     *
+     * Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+     * tempor incididunt ut labore et dolore magna aliqua.
+     *
+     */
+    ilkPrice: CurrencyRatio;
+
+    /**
+     * Lorem ipsum.
+     *
+     * Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+     * tempor incididunt ut labore et dolore magna aliqua.
+     *
+     */
+    encumberedCollateral: Currency;
+
+    /**
+     * Lorem ipsum.
+     *
+     * Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+     * tempor incididunt ut labore et dolore magna aliqua.
+     *
+     */
+    encumberedDebt: Currency;
+  }
+
   interface WatchInterfaceMcd {
 
     /** Watch the total encumbered debt of a collateral type
@@ -108,9 +151,25 @@ declare global {
     /**
      * @param ilkName String uniquely identifying an ilk
      */
-    maxAuctionLotSize(
+    static maxAuctionLotSize(
       ilkName: string
       ): BigNumber;
+
+    /**
+     * Get a vault by id.
+     *
+     * Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+     * tempor incididunt ut labore et dolore magna aliqua.
+     *
+     * ```js
+     * const vault = watch.vaultById(2)
+     * ```
+     *
+     * @param id Numerical id of the vault
+     */
+    static vaultById(
+      id: number
+      ): VaultResult;
   }
 }
 
