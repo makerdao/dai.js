286d0fc7725a0a816358b2c60eeb491141c2145e
Tyler Sorensen <tyler@makerdao.com>
2020-01-29 18:07:43 -0800

    add current dsspell abi and getEta() function

--- packages/dai-plugin-governance/src/PauseService.js
+++ packages/dai-plugin-governance/src/PauseService.js
@@ -1,5 +1,6 @@
 import { PublicService } from '@makerdao/services-core';
 import { PAUSE } from './utils/constants';
+import DsSpellAbi from '../contracts/abis/DSSpell.json';
 
 export default class PauseService extends PublicService {
   constructor(name = 'pause') {
@@ -10,6 +11,15 @@ export default class PauseService extends PublicService {
     if (this.delay) return this.delay;
     this.delay = this._pauseContract().delay();
     return this.delay;
+  }
+
+  async getEta(spellAddress) {
+    const spell = this.get('smartContract').getContractByAddressAndAbi(
+      spellAddress,
+      DsSpellAbi
+    );
+    const eta = await spell.eta();
+    return new Date(eta.toNumber() * 1000);
   }
 
   refresh() {
--- packages/dai-plugin-governance/test/PauseService.test.js
+++ packages/dai-plugin-governance/test/PauseService.test.js
@@ -34,3 +34,9 @@ test('get delay', async () => {
   const delay = await pauseService.getDelayInSeconds();
   expect(delay.toNumber()).toBe(1);
 });
+
+//currently this test works for mainnet
+xtest('get eta of hat', async () => {
+  const eta = await pauseService.getEta('0xf880d43bb9a32dd212c77b82a7336be31ecaee08');
+  expect(eta).toEqual(new Date(1580039599000));
+});
