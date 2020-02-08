637f84f3fa1874725c9f2bb561d0ac44464f3cd2
Tyler Sorensen <tyler@makerdao.com>
2020-02-07 21:46:53 -0800

    comments in SpellService.test

--- packages/dai-plugin-governance/test/SpellService.test.js
+++ packages/dai-plugin-governance/test/SpellService.test.js
@@ -7,6 +7,7 @@ import SpellService from '../src/SpellService';
 
 let maker, spellService;
 beforeAll(async () => {
+  //TODO: update these tests to use the testchain instead of mainnet
   maker = await setupTestMakerInstance('mainnet');
   spellService = maker.service('spell');
 });
@@ -32,26 +33,24 @@ test('can create spell Service', async () => {
 
 test('get spell execution date', async () => {
   const date = await spellService.getExecutionDate(
-      '0x48916a2b11fa7a895426eedf9acf2d70523b1677'
-    );
+    '0x48916a2b11fa7a895426eedf9acf2d70523b1677'
+  );
   expect(date).toEqual(new Date('2020-02-04T11:35:48.000Z'));
 });
 
+//Note: this test currently uses mainnet, so if it starts failing, it is probably because the pause delay on mainnet has been increased from 0
 test('get delay', async () => {
   const delay = await spellService.getDelayInSeconds();
   expect(delay.toNumber()).toBe(0);
 });
 
-//currently this test works for mainnet
 test('get spell eta', async () => {
   const eta = await spellService.getEta(
     '0xf880d43bb9a32dd212c77b82a7336be31ecaee08'
   );
-  console.log('eta', eta);
   expect(eta).toEqual(new Date('2020-01-26T11:53:19.000Z'));
 });
 
-//currently this test works for mainnet
 test('get spell done boolean', async () => {
   const done = await spellService.getDone(
     '0xf880d43bb9a32dd212c77b82a7336be31ecaee08'
@@ -59,7 +58,6 @@ test('get spell done boolean', async () => {
   expect(done).toBe(true);
 });
 
-//currently this test works for mainnet
 test('get spell action address', async () => {
   const action = await spellService.getAction(
     '0xf880d43bb9a32dd212c77b82a7336be31ecaee08'
