a9832042d810bd0b5f140c578ede219289e562cb
sirromdev <sirromdev@gmail.com>
2020-02-18 15:59:31 +0000

    make null return optional in proxyAddress schema

--- packages/dai-plugin-mcd/src/schemas/proxyRegistry.js
+++ packages/dai-plugin-mcd/src/schemas/proxyRegistry.js
@@ -1,15 +1,19 @@
 import { PROXY_ADDRESS, PROXY_OWNER } from './constants';
 
 export const proxyRegistryProxies = {
-  generate: address => ({
+  generate: (address, nullIfNoProxy = false) => ({
     id: `PROXY_REGISTRY.proxies(${address})`,
     contract: 'PROXY_REGISTRY',
     call: ['proxies(address)(address)', address],
     transforms: {
-      [PROXY_ADDRESS]: proxyAddress =>
-        proxyAddress === '0x0000000000000000000000000000000000000000'
-          ? null
-          : proxyAddress
+      [PROXY_ADDRESS]: proxyAddress => {
+        if (nullIfNoProxy) {
+          return proxyAddress === '0x0000000000000000000000000000000000000000'
+            ? null
+            : proxyAddress;
+        }
+        return proxyAddress;
+      }
     }
   }),
   returns: [[PROXY_ADDRESS]]
