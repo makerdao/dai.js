d7fb5cc50675612ec184a82ca91f3b35eb1bbfef
shkfnly <ashoka.finley@gmail.com>
2020-02-24 17:07:28 -0500

    Continuing work to build a separate module

--- packages/dai-plugin-scd/src/index.js
+++ packages/dai-plugin-scd/src/index.js
@@ -1,34 +1,27 @@
-imp
+imp;
 
-import Cdp from './Cdp'
-import EthereumCdpService from './EthereumCdpService'
-import PriceService from './PriceService'
-import ProxyCdp from './ProxyCdp'
-import QueryApiScdService from './QueryApiScdService'
-import { ServiceRoles as ServiceRoles_ } from './utils/constants'
+import Cdp from './Cdp';
+import EthereumCdpService from './EthereumCdpService';
+import PriceService from './PriceService';
+import ProxyCdp from './ProxyCdp';
+import QueryApiScdService from './QueryApiScdService';
+import { ServiceRoles as ServiceRoles_ } from './utils/constants';
 
 export const ServiceRoles = ServiceRoles_;
-const {
-  CDP,
-  PRICE,
-  PROXY_CDP,
-  QUERY_API
-} = ServiceRoles;
+const { CDP, PRICE, PROXY_CDP, QUERY_API } = ServiceRoles;
 
 export default {
-  addConfig: function(config, { network = "mainnet", staging = false }) {
+  addConfig: function(config, { network = 'mainnet', staging = false }) {
     const contractAddresses = {
       kovan: require('../contracts/addresses/kovan.json'),
       mainnet: require('../contracts/addresses/mainnet.json')
-    }
+    };
 
     try {
-      contractAddresses.testnet = require('../contracts/addresses/testnet.json')
-    } catch (err) {
+      contractAddresses.testnet = require('../contracts/addresses/testnet.json');
+    } catch (err) {}
 
-    }
-
-    const addressKey = network === 'ganache' ? 'testnet' : network
+    const addressKey = network === 'ganache' ? 'testnet' : network;
 
     // const addContracts = {
     //   [CDP]: {
@@ -38,14 +31,10 @@ export default {
     // }
     const makerConfig = {
       ...config,
-      additionalServices: [
-        'CDP',
-        'PRICE',
-        'QUERY_API'
-      ],
+      additionalServices: ['CDP', 'PRICE', 'QUERY_API'],
       ['CDP']: EthereumCdpService,
       ['PRICE']: PriceService
-    }
-    return makerConfig
+    };
+    return makerConfig;
   }
-}
+};
