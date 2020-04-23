e81a27ddaf19110039939f689cc533a3394cb3da
Ashoka Finley <ashoka.finley@example.com>
2020-04-23 17:45:37 -0400

    updated contracts using script

--- packages/dai-plugin-scd/scripts/install-testchain-outputs.sh
+++ packages/dai-plugin-scd/scripts/install-testchain-outputs.sh
@@ -1,14 +1,20 @@
 #!/usr/bin/env bash
 set -e
 
-# TODO take the source directory as a parameter
-
 CWD=`dirname $0`
 CONTRACTS=$CWD/../contracts
 SOURCE=${1:-$CWD/../../../node_modules/@makerdao/testchain}
 
-for file in $SOURCE/out/mcd/*.abi; do
+function jq_inplace {
+  TMP=$(mktemp)
+  jq "$1" > $TMP && mv $TMP "$2"
+}
+
+for file in $SOURCE/out/*.abi; do
   cp $file $CONTRACTS/abis/$(basename $file .abi).json
 done
 
-cp $SOURCE/out/addresses-mcd.json $CONTRACTS/addresses/testnet.json
+cp $SOURCE/out/addresses.json $CONTRACTS/addresses/testnet.json
+MULTICALL=$(cat $SOURCE/out/addresses-mcd.json | jq '.MULTICALL')
+
+cat $CONTRACTS/addresses/testnet.json | jq_inplace ".MULTICALL = $(echo $MULTICALL)" $CONTRACTS/addresses/testnet.json
