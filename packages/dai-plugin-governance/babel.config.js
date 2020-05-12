8fe4cea7f479b3dc9956c2e10e1de9e81ce046e6
Josh Levine <joshuapark95@gmail.com>
2020-05-12 18:34:48 -0400

    dai-plugin-governance-v0.12.3

--- packages/dai-plugin-governance/babel.config.js
+++ packages/dai-plugin-governance/babel.config.js
@@ -1,1 +1,22 @@
-../../babel.config.js
+module.exports = function(api) {
+  api.cache(true);
+
+  const presets = ['@babel/preset-env'];
+  const plugins = [
+    ['@babel/plugin-proposal-decorators', { legacy: true }],
+    '@babel/plugin-proposal-function-sent',
+    '@babel/plugin-proposal-export-namespace-from',
+    '@babel/plugin-proposal-numeric-separator',
+    '@babel/plugin-proposal-throw-expressions',
+    '@babel/plugin-proposal-class-properties',
+    '@babel/plugin-transform-runtime'
+  ];
+
+  const babelrcRoots = ['.', './lib/*'];
+
+  return {
+    presets,
+    plugins,
+    babelrcRoots
+  };
+};
