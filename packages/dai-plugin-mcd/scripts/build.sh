07f826dfb85ed8e010da26460ac9406c52050cdc
Josh Levine <joshuapark95@gmail.com>
2020-05-21 21:28:57 -0400

    dai-plugin-mcd-v1.4.5

---
+++ packages/dai-plugin-mcd/scripts/build.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+set -e
+
+if [ "$1" = "dirty" ]; then
+  echo "Dirty mode: not removing previous build files."
+else
+  rm -rf dist
+fi
+
+babel contracts --out-dir ./dist/contracts
+babel src --out-dir ./dist/src
+
+copyfiles \
+  contracts/abis/* \
+  contracts/abiMap.json \
+  contracts/addresses/* \
+  dist
