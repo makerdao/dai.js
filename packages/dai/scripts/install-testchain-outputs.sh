135486e9478a66f9eb12bf9fe7d60dbf015179b3
Ethan Bennett <ebennett19@gmail.com>
2020-07-07 02:24:34 -0400

    testchain v1.1.9; add SAI prefixes in dai output script

--- packages/dai/scripts/install-testchain-outputs.sh
+++ packages/dai/scripts/install-testchain-outputs.sh
@@ -2,12 +2,18 @@
 set -e
 
 CONTRACTS=$DAI/contracts
+TESTNET_ADDRESSES=$CONTRACTS/addresses/testnet.json
 
 for file in $SOURCE/out/*.abi; do
   cp $file $CONTRACTS/abis/$(basename $file .abi).json
 done
 
-cp $SOURCE/out/addresses.json $CONTRACTS/addresses/testnet.json
+cp $SOURCE/out/addresses.json $TESTNET_ADDRESSES
 MULTICALL=$(cat $SOURCE/out/addresses-mcd.json | jq '.MULTICALL')
 
-cat $CONTRACTS/addresses/testnet.json | jq_inplace ".MULTICALL = $(echo $MULTICALL)" $CONTRACTS/addresses/testnet.json
+cat $TESTNET_ADDRESSES | jq_inplace ".MULTICALL = $(echo $MULTICALL)" $TESTNET_ADDRESSES
+
+for CONTRACT in "GEM" "GOV" "PIP" "PEP" "PIT" "ADM" "SIN" "SKR" "DAD" "MOM" "VOX" "TUB" "TAP" "TOP"
+do
+  cat $TESTNET_ADDRESSES | jq_inplace ".SAI_$CONTRACT = .$CONTRACT | del(.$CONTRACT)" $TESTNET_ADDRESSES
+done
