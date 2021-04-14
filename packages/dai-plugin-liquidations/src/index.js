97544a5bc67d34ef1f709f343e40b22d30ac2d08
Phil Bain <bain.pmcg@gmail.com>
2021-04-14 11:18:40 -0600

    add liq contracts and abis to plugin

--- packages/dai-plugin-liquidations/src/index.js
+++ packages/dai-plugin-liquidations/src/index.js
@@ -1,13 +1,36 @@
+import { map, prop } from 'ramda';
 import LiquidationService from './LiquidationService';
 
 export const MCD_CLIP_LINK_A = 'MCD_CLIP_LINK_A';
+export const MCD_DOG = 'MCD_DOG';
 
 export default {
   addConfig: function(config) {
+    const contractAddresses = {
+      kovan: require('../contracts/addresses/kovan.json'),
+      mainnet: require('../contracts/addresses/mainnet.json')
+    };
+    try {
+      contractAddresses.testnet = require('../contracts/addresses/testnet.json');
+    } catch (err) {
+      // do nothing here; throw an error only if we later attempt to use ganache
+    }
+    const addContracts = {
+      [MCD_CLIP_LINK_A]: {
+        address: map(prop('MCD_CLIP_LINK_A'), contractAddresses),
+        abi: require('../contracts/abis/Clipper.json')
+      },
+      [MCD_DOG]: {
+        address: map(prop('MCD_DOG'), contractAddresses),
+        abi: require('../contracts/abis/Dog.json')
+      }
+    };
+
     return {
       ...config,
       additionalServices: ['liquidation'],
-      liquidation: LiquidationService
+      liquidation: LiquidationService,
+      smartContract: { addContracts }
     };
   }
 };
