80d3a37b5c77072e52e50a491859952318d5e937
adamgoth <adamgoth@gmail.com>
2021-05-10 10:48:16 -0400

    Add new clips to liq config

--- packages/dai-plugin-liquidations/src/index.js
+++ packages/dai-plugin-liquidations/src/index.js
@@ -1,13 +1,23 @@
 import { map, prop } from 'ramda';
 import LiquidationService from './LiquidationService';
 
-export const MCD_CLIP_LINK_A = 'MCD_CLIP_LINK_A';
-export const MCD_CLIP_YFI_A = 'MCD_CLIP_YFI_A';
-export const MCD_CLIP_WBTC_A = 'MCD_CLIP_WBTC_A';
+export const MCD_DOG = 'MCD_DOG';
+export const MCD_CLIP_AAVE_A = 'MCD_CLIP_AAVE_A';
+export const MCD_CLIP_BAL_A = 'MCD_CLIP_BAL_A';
+export const MCD_CLIP_BAT_A = 'MCD_CLIP_BAT_A';
+export const MCD_CLIP_COMP_A = 'MCD_CLIP_COMP_A';
 export const MCD_CLIP_ETH_A = 'MCD_CLIP_ETH_A';
 export const MCD_CLIP_ETH_B = 'MCD_CLIP_ETH_B';
 export const MCD_CLIP_ETH_C = 'MCD_CLIP_ETH_C';
-export const MCD_DOG = 'MCD_DOG';
+export const MCD_CLIP_KNC_A = 'MCD_CLIP_KNC_A';
+export const MCD_CLIP_LINK_A = 'MCD_CLIP_LINK_A';
+export const MCD_CLIP_LRC_A = 'MCD_CLIP_LRC_A';
+export const MCD_CLIP_MANA_A = 'MCD_CLIP_MANA_A';
+export const MCD_CLIP_RENBTC_A = 'MCD_CLIP_RENBTC_A';
+export const MCD_CLIP_UNI_A = 'MCD_CLIP_UNI_A';
+export const MCD_CLIP_WBTC_A = 'MCD_CLIP_WBTC_A';
+export const MCD_CLIP_YFI_A = 'MCD_CLIP_YFI_A';
+export const MCD_CLIP_ZRX_A = 'MCD_CLIP_ZRX_A';
 
 export default {
   addConfig: function(config, { vulcanize = true }) {
@@ -21,16 +31,24 @@ export default {
       // do nothing here; throw an error only if we later attempt to use ganache
     }
     const addContracts = {
-      [MCD_CLIP_LINK_A]: {
-        address: map(prop('MCD_CLIP_LINK_A'), contractAddresses),
+      [MCD_DOG]: {
+        address: map(prop('MCD_DOG'), contractAddresses),
+        abi: require('../contracts/abis/Dog.json')
+      },
+      [MCD_CLIP_AAVE_A]: {
+        address: map(prop('MCD_CLIP_AAVE_A'), contractAddresses),
         abi: require('../contracts/abis/Clipper.json')
       },
-      [MCD_CLIP_YFI_A]: {
-        address: map(prop('MCD_CLIP_YFI_A'), contractAddresses),
+      [MCD_CLIP_BAL_A]: {
+        address: map(prop('MCD_CLIP_BAL_A'), contractAddresses),
         abi: require('../contracts/abis/Clipper.json')
       },
-      [MCD_CLIP_WBTC_A]: {
-        address: map(prop('MCD_CLIP_WBTC_A'), contractAddresses),
+      [MCD_CLIP_BAT_A]: {
+        address: map(prop('MCD_CLIP_BAT_A'), contractAddresses),
+        abi: require('../contracts/abis/Clipper.json')
+      },
+      [MCD_CLIP_COMP_A]: {
+        address: map(prop('MCD_CLIP_COMP_A'), contractAddresses),
         abi: require('../contracts/abis/Clipper.json')
       },
       [MCD_CLIP_ETH_A]: {
@@ -45,9 +63,41 @@ export default {
         address: map(prop('MCD_CLIP_ETH_C'), contractAddresses),
         abi: require('../contracts/abis/Clipper.json')
       },
-      [MCD_DOG]: {
-        address: map(prop('MCD_DOG'), contractAddresses),
-        abi: require('../contracts/abis/Dog.json')
+      [MCD_CLIP_KNC_A]: {
+        address: map(prop('MCD_CLIP_KNC_A'), contractAddresses),
+        abi: require('../contracts/abis/Clipper.json')
+      },
+      [MCD_CLIP_LINK_A]: {
+        address: map(prop('MCD_CLIP_LINK_A'), contractAddresses),
+        abi: require('../contracts/abis/Clipper.json')
+      },
+      [MCD_CLIP_LRC_A]: {
+        address: map(prop('MCD_CLIP_LRC_A'), contractAddresses),
+        abi: require('../contracts/abis/Clipper.json')
+      },
+      [MCD_CLIP_MANA_A]: {
+        address: map(prop('MCD_CLIP_MANA_A'), contractAddresses),
+        abi: require('../contracts/abis/Clipper.json')
+      },
+      [MCD_CLIP_RENBTC_A]: {
+        address: map(prop('MCD_CLIP_RENBTC_A'), contractAddresses),
+        abi: require('../contracts/abis/Clipper.json')
+      },
+      [MCD_CLIP_UNI_A]: {
+        address: map(prop('MCD_CLIP_UNI_A'), contractAddresses),
+        abi: require('../contracts/abis/Clipper.json')
+      },
+      [MCD_CLIP_WBTC_A]: {
+        address: map(prop('MCD_CLIP_WBTC_A'), contractAddresses),
+        abi: require('../contracts/abis/Clipper.json')
+      },
+      [MCD_CLIP_YFI_A]: {
+        address: map(prop('MCD_CLIP_YFI_A'), contractAddresses),
+        abi: require('../contracts/abis/Clipper.json')
+      },
+      [MCD_CLIP_ZRX_A]: {
+        address: map(prop('MCD_CLIP_ZRX_A'), contractAddresses),
+        abi: require('../contracts/abis/Clipper.json')
       }
     };
 
